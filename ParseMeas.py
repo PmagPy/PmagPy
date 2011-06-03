f0ef690fc4c9f81e4acda40ad59621fc6e840cf2
Lisa Tauxe <ltauxe@ucsd.edu>
2011-06-03 16:02:10 -0700

    new file:   ParseMeas.py # calls function to extract specimen information from magic_measurements.txt file, writes er_specimens.txt 	new file:   ReorderSamples.py # calls function to re-order er_samples.txt file based on orientation selected in zeq_specimens.txt

---
+++ ParseMeas.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys,pmag,math
+#
+#
+def main():
+    """
+    NAME
+        ParseMeas.py
+
+    DESCRIPTION
+        takes measurments file and creates specimen and instrument files
+
+    SYNTAX
+        ParseMeas.py [command line options]
+
+    OPTIONS
+        -h prints help message and quits
+        -f: magic_measurements input file, default is "magic_measurements.txt"
+        -fsi: er_sites input file, default is "er_sites.txt"
+        -Fsp: specimen output er_specimens format file, default is "er_specimens.txt"
+        -Fin: instrument output  magic_instruments format file, default is "magic_instruments.txt"
+    OUPUT
+        writes er_specimens and magic_instruments formatted files
+
+    """
+    infile='magic_measurements.txt'
+    sitefile="er_sites.txt"
+    specout="er_specimens.txt"
+    instout="magic_instruments.txt"
+# get command line stuff
+    if "-h" in sys.argv:
+	print main.__doc__
+        sys.exit()
+    if '-f' in sys.argv:
+	ind=sys.argv.index("-f")
+	infile=sys.argv[ind+1]
+    if '-fsi' in sys.argv:
+	ind=sys.argv.index("-fsi")
+	sitefile=sys.argv[ind+1]
+    if '-Fsp' in sys.argv:
+	ind=sys.argv.index("-Fsp")
+	specout=sys.argv[ind+1]
+    if '-Fin' in sys.argv:
+	ind=sys.argv.index("-Fin")
+	instout=sys.argv[ind+1]
+    if '-WD' in sys.argv:
+        ind=sys.argv.index("-WD")
+        dir_path=sys.argv[ind+1]
+        infile=dir_path+'/'+infile
+        sitefile=dir_path+'/'+sitefile
+        specout=dir_path+'/'+specout
+        instout=dir_path+'/'+instout
+# now do re-ordering 
+    pmag.ParseMeasFile(infile,sitefile,instout,specout)
+main()
