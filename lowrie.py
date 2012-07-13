996353583e4f9f89c84b1a4dd5d5692292c3737f
Lisa Tauxe <ltauxe@ucsd.edu>
2012-07-13 15:41:32 -0700

    modified:   lowrie.py

--- lowrie.py
+++ lowrie.py
@@ -22,18 +22,25 @@ def main():
     FIG={} # plot dictionary
     FIG['lowrie']=1 # demag is figure 1
     pmagplotlib.plot_init(FIG['lowrie'],6,6)
-    XLP=""
-    norm=1
+    norm=1 # default is to normalize by maximum axis
     if len(sys.argv)>1:
         if '-h' in sys.argv:
             print main.__doc__
             sys.exit()
-        if '-N' in sys.argv: norm=0
-        if '-f' in sys.argv:
+        if '-N' in sys.argv: norm=0 # don't normalize
+        if '-f' in sys.argv: # sets input filename
             ind=sys.argv.index("-f")
             in_file=sys.argv[ind+1]
-    data=open(in_file).readlines()
-    PmagRecs=[]
+        else:
+            print main.__doc__
+            print 'you must supply a file name'
+            sys.exit() 
+    else:
+        print main.__doc__
+        print 'you must supply a file name'
+        sys.exit() 
+    data=open(in_file).readlines() # open the SIO format file
+    PmagRecs=[] # set up a list for the results
     keys=['specimen','treatment','csd','M','dec','inc']
     for line in data:
         PmagRec={}
@@ -53,19 +60,15 @@ def main():
             DIMs.append([float(dat['dec']),float(dat['inc']),float(dat['M'])])
             Temps.append(float(dat['treatment']))
         carts=pmag.dir2cart(DIMs).transpose()
-        if norm==1:
-            nrm=0
-            m1,m2,m3=max(abs(carts[0])),max(abs(carts[1])),max(abs(carts[2]))
-            if m1>m2:nrm=m1
-            if m2>nrm:nrm=m2
-            if m3>nrm:nrm=m3
-        else: nrm=1.
+        if norm==1: # want to normalize
+            nrm=max(max(abs(carts[0])),max(abs(carts[1])),max(abs(carts[2]))) # by maximum of x,y,z values
+        else: nrm=1. # don't normalize
         pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[0])/nrm,sym='r-')
-        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[0])/nrm,sym='ro')
-        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[1])/nrm,sym='b-')
-        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[1])/nrm,sym='bs')
-        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[2])/nrm,sym='m-')
-        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[2])/nrm,sym='m^',title=spc)
+        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[0])/nrm,sym='ro') # X direction
+        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[1])/nrm,sym='c-')
+        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[1])/nrm,sym='cs') # Y direction
+        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[2])/nrm,sym='k-')
+        pmagplotlib.plotXY(FIG['lowrie'],Temps,abs(carts[2])/nrm,sym='k^',title=spc) # Z direction
         pmagplotlib.drawFIGS(FIG)
         ans=raw_input('Save figure? y/[n]')
         if ans=='y':
