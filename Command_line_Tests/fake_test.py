509d9347e11e82342af7317534480470d7cd2c7a
Lori J <mintblue87@gmail.com>
2014-03-14 20:25:54 -0700

    attempting first push to travis

---
+++ Command_line_Tests/fake_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# fake_test.py: -*- Python -*-  DESCRIPTIVE TEXT.
+# 
+#  Copyright (c) 2014 Lori Jonestrask
+#  Author: Lori Jonestrask (mintblue87@gmail.com) .
+
+
+from scripttest import TestFileEnvironment
+env = TestFileEnvironment('./new-test-output')
+import unittest
+
+print 'hello'
+    
