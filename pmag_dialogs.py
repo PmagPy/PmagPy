74cb9ad626df6163ae4878be89bfdcf06343c7b7
Lori J <mintblue87@gmail.com>
2014-04-25 17:02:33 -0700

    IODP basically works

--- pmag_dialogs.py
+++ pmag_dialogs.py
@@ -104,6 +104,8 @@ class import_magnetometer_data(wx.Dialog):
             dia = convert_HUJI_files_to_MagIC(self.WD)
         elif file_type == 'LDEO':
             dia = convert_LDEO_files_to_MagIC(self.WD)
+        elif file_type == 'IODP':
+            dia = convert_IODP_csv_files_to_MagIC(self.WD)
         dia.Center()
         dia.Show()
 
@@ -1320,6 +1322,117 @@ class convert_LDEO_files_to_MagIC(wx.Frame):
 
     def on_helpButton(self, event):
         pw.on_helpButton("LDEO_magic.py -h")
+
+
+
+
+class convert_IODP_csv_files_to_MagIC(wx.Frame):
+
+    """ """
+    title = "PmagPy IODP csv conversion"
+
+    def __init__(self,WD):
+        wx.Frame.__init__(self, None, wx.ID_ANY, self.title)
+        self.panel = wx.ScrolledWindow(self)
+        self.WD = WD
+        self.InitUI()
+
+    def InitUI(self):
+
+        pnl = self.panel
+
+        TEXT = "IODP csv format file"
+        bSizer_info = wx.BoxSizer(wx.HORIZONTAL)
+        bSizer_info.Add(wx.StaticText(pnl, label=TEXT), wx.ALIGN_LEFT)
+
+        #---sizer 0 ----
+        self.bSizer0 = pw.choose_file(pnl, 'add', method = self.on_add_file_button)
+
+        #---sizer 1 ----
+        self.bSizer1 = pw.replicate_measurements(pnl)
+        
+        #---sizer 2 ----
+
+        #---sizer 3 ----
+
+        #---sizer 4 ----
+
+        #---sizer 5 ---
+
+        #---sizer 6 ----
+
+        #---sizer 7 ---
+
+
+        #---buttons ---
+        self.okButton = wx.Button(pnl, wx.ID_OK, "&OK")
+        self.Bind(wx.EVT_BUTTON, self.on_okButton, self.okButton)
+
+        self.cancelButton = wx.Button(pnl, wx.ID_CANCEL, '&Cancel')
+        self.Bind(wx.EVT_BUTTON, self.on_cancelButton, self.cancelButton)
+
+        self.helpButton = wx.Button(pnl, wx.ID_ANY, '&Help')
+        self.Bind(wx.EVT_BUTTON, self.on_helpButton, self.helpButton)
+
+        hboxok = wx.BoxSizer(wx.HORIZONTAL)
+        hboxok.Add(self.okButton, 0, wx.ALL, 5)
+        hboxok.Add(self.cancelButton, 0, wx.ALL, 5)
+        hboxok.Add(self.helpButton, 0, wx.ALL, 5)
+
+        #------
+        vbox=wx.BoxSizer(wx.VERTICAL)
+
+        vbox.AddSpacer(10)
+        vbox.Add(bSizer_info, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        vbox.Add(self.bSizer0, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        vbox.Add(self.bSizer1, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        #vbox.Add(self.bSizer2, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        #vbox.Add(self.bSizer3, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        #vbox.Add(self.bSizer4, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        #vbox.Add(self.bSizer5, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        #vbox.Add(self.bSizer6, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        #vbox.Add(self.bSizer7, flag=wx.ALIGN_LEFT|wx.TOP, border=10)
+        #vbox.AddSpacer(10)
+        #vbox.Add(wx.StaticLine(pnl), 0, wx.ALL|wx.EXPAND, 5)
+        vbox.Add(hboxok, flag=wx.ALIGN_CENTER)        
+        vbox.AddSpacer(20)
+
+        hbox_all= wx.BoxSizer(wx.HORIZONTAL)
+        hbox_all.AddSpacer(20)
+        hbox_all.AddSpacer(vbox)
+        hbox_all.AddSpacer(20)
+        
+        self.panel.SetSizer(hbox_all)
+        self.panel.SetScrollbars(20, 20, 50, 50)
+        hbox_all.Fit(self)
+        self.Show()
+        self.Centre()
+
+
+    def on_add_file_button(self,event):
+        text = "choose file to convert to MagIC"
+        pw.on_add_file_button(self.panel, self.WD, event, text)
+
+    def on_okButton(self, event):
+        wd = self.WD
+        full_file = self.bSizer0.return_value()
+        index = full_file.rfind('/')
+        IODP_file = full_file[index+1:]
+        outfile = IODP_file + ".magic"
+        replicate = self.bSizer1.return_value()
+        if replicate:
+            replicate = ''
+        else:
+            replicate = "-A"
+        COMMAND = "IODP_csv_magic.py -WD {0} -f {1} -F {2} {3}".format(wd, IODP_file, outfile, replicate)
+        print COMMAND
+        pw.run_command_and_close_window(self, COMMAND, outfile)
+
+    def on_cancelButton(self,event):
+        self.Destroy()
+
+    def on_helpButton(self, event):
+        pw.on_helpButton("-h")
 
 
 
