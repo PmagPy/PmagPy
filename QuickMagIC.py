95b755ecfdcba73c0b1071bd8231d53d12313cd6
Lori J <mintblue87@gmail.com>
2014-06-19 10:14:50 -0700

    rename pmag_dialogs to pmag_basic_dialogs (eventually will distinguish between that and pmag_menu_dialogs)

--- QuickMagIC.py
+++ QuickMagIC.py
@@ -5,7 +5,7 @@ import wx.lib.buttons as buttons
 import os
 import sys
 import pmag
-import pmag_dialogs
+import pmag_basic_dialogs
 #import pmag_menu
 
 
@@ -238,7 +238,7 @@ class MagMainFrame(wx.Frame):
         os.system(outstring)
         
     def on_convert_file(self,event):
-        pmag_dialogs_dia=pmag_dialogs.import_magnetometer_data(self, wx.ID_ANY, '',self.WD)
+        pmag_dialogs_dia=pmag_basic_dialogs.import_magnetometer_data(self, wx.ID_ANY, '',self.WD)
         pmag_dialogs_dia.Show()
         pmag_dialogs_dia.Center()
 
@@ -326,7 +326,7 @@ class MagMainFrame(wx.Frame):
         SIZE=wx.DisplaySize()
         SIZE=(SIZE[0]-0.1*SIZE[0],SIZE[1]-0.1*SIZE[1])
         Data,Data_hierarchy=self.get_data()
-        frame = pmag_dialogs.OrientFrameGrid (None, -1, 'demag_orient.txt',self.WD,Data_hierarchy,SIZE)        
+        frame = pmag_basic_dialogs.OrientFrameGrid (None, -1, 'demag_orient.txt',self.WD,Data_hierarchy,SIZE)        
         frame.Show(True)
         frame.Centre()
 
