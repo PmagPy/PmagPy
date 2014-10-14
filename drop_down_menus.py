526de43c1fc0df8fb294fe9b862b93fb3fb3fa08
Lori J <mintblue87@gmail.com>
2014-10-13 19:31:40 -0700

    QuickMagic step 3: fix some more column settings problems in drop-down-menus

--- drop_down_menus.py
+++ drop_down_menus.py
@@ -42,7 +42,7 @@ class Menus():
             return 0
         if col == 2 and self.data_type == 'age':
             return 0
-        if (col not in (-1, 0, 1)) or (col == 1 and self.data_type == 'location'):
+        if (col not in (-1, 0, 1)) or (col == 1 and self.data_type in ['location', 'age']):
         # if a new column was chosen without de-selecting the previous column, deselect the old selected_col
             if self.selected_col != None and self.selected_col != col: 
                 col_label_value = self.grid.GetColLabelValue(self.selected_col)
@@ -65,17 +65,17 @@ class Menus():
                 for row in range(self.grid.GetNumberRows()):
                     self.grid.SetCellBackgroundColour(row, col, 'light blue')
                 self.grid.ForceRefresh()
-        has_dropdown = ((col == 2 and self.data_type is 'specimen') or (col in range(2, 6) and self.data_type in ['site', 'sample']) or (col in (3, 5) and self.data_type == 'age') or (col == 6 and self.data_type == 'site') or (col == 1 and self.data_type == 'location'))
+        has_dropdown = ((col == 2 and self.data_type is 'specimen') or (col in range(2, 6) and self.data_type in ['site', 'sample']) or (col in (3, 5) and self.data_type == 'age') or (col == 6 and self.data_type == 'site') or (col == 1 and self.data_type in ['location']))
 
         # if the column has no drop-down list, allow user to edit all cells in the column through text entry
-        if not has_dropdown and col not in (0, 1):  
+        if (not has_dropdown and col not in (0, 1)) or (col == 1 and self.data_type in ['age']):  
             if self.selected_col == col:
                 self.check.changes = True
                 default_value = self.grid.GetCellValue(0, col)
                 #data = wx.GetTextFromUser("Enter value for all cells in the column\nNote: this will overwrite any existing cell values", "Edit All", default_value)
-                
                 data = None
                 dialog = wx.TextEntryDialog(None, "Enter value for all cells in the column\nNote: this will overwrite any existing cell values", "Edit All", default_value, style=wx.OK|wx.CANCEL)
+                dialog.Centre()
                 if dialog.ShowModal() == wx.ID_OK: 
                     data = dialog.GetValue() 
                     for row in range(self.grid.GetNumberRows()):
@@ -88,7 +88,6 @@ class Menus():
                     self.grid.SetCellBackgroundColour(row, col, 'white')
                 self.grid.ForceRefresh()
                 self.selected_col = None
-
 
             
     def clean_up(self, grid):
@@ -104,14 +103,23 @@ class Menus():
         """creates popup menu when user clicks on the column
         if that column is in the list of choices that get a drop-down menu.
         allows user to edit the column, but only from available values"""
+        #print dir(event)
+        #print "event.WasProcessed()", event.WasProcessed()
+        #print "event.Position", event.Position
+        #print "event.ResumePropagation", event.ResumePropagation() # requires level argument, or something
+        #event.Skip() # doesn't seem to do anything useful
+        #event.Veto() # prevents border from being drawn
         col = event.GetCol()
+        row = event.GetRow()
+
+        #print "row", row
+        #print "col", col
+        #self.grid.SetGridCursor(row, col) # causes infinite recursion error
         if col in choices.keys(): # column should have a pop-up menu
-            row = event.GetRow()
             menu = wx.Menu()
             two_tiered = choices[col][1]
             choices = choices[col][0]
             if not two_tiered: # menu is one tiered
-                # insert CLEAR option as first choice
                 if 'CLEAR cell of all values' not in choices:
                     choices.insert(0, 'CLEAR cell of all values')
                 for choice in choices:
@@ -121,7 +129,6 @@ class Menus():
                 self.window.PopupMenu(menu)
                 menu.Destroy()
             else: # menu is two_tiered
-                # insert CLEAR option as first menuitem
                 clear = menu.Append(-1, 'CLEAR cell of all values')
                 self.window.Bind(wx.EVT_MENU, lambda event: self.on_select_menuitem(event, grid, row, col), clear)
                 for choice in sorted(choices.items()):
@@ -135,7 +142,6 @@ class Menus():
 
     def update_drop_down_menu(self, grid, choices):
         self.window.Bind(wx.grid.EVT_GRID_SELECT_CELL, lambda event: self.on_left_click(event, grid, choices), grid) 
-
 
     def on_select_menuitem(self, event, grid, row, col):
         """
