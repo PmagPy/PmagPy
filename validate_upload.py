6afee14ee2551a60f5df07b9f80a7c5911641eef
Lori J <mintblue87@gmail.com>
2014-12-09 16:07:15 -0800

    fix validate_upload: certain col names are required in one file, but not in another

--- validate_upload.py
+++ validate_upload.py
@@ -12,7 +12,17 @@ def get_data_model():
     """
     try to grab the up to date data model document from the EarthRef site.
     if that fails, try to get the data model document from the PmagPy directory on the user's computer.
-    if that fails, return False
+    if that fails, return False.
+    data_model is a set of nested dictionaries that looks like this:
+    {'magic_contributions': 
+        {'group_userid': {'data_status': 'Optional', 'data_type': 'String(10)'}, 'activate': {'data_status': 'Optional', 'data_type': 'String(1)'}, ....}, 
+    'er_synthetics':
+        {'synthetic_type': {'data_status': 'Required', 'data_type': 'String(50)'}, 'er_citation_names': {'data_status': 'Required', 'data_type': 'List(500)'}, ...},
+    ....
+    }
+    the top level keys are the file types.  
+    the second level keys are the possible headers for that file type.
+    the third level keys are data_type and data_status for that header.
     """
     print "getting data model, please be patient"
     url = 'http://earthref.org/services/MagIC-data-model.txt'
@@ -28,10 +38,20 @@ def get_data_model():
             return False
 
     data_model = pmag.magic_read(None, data)
-    ref_dicts = [d for d in data_model[0] if d['column_nmb'] != '>>>>>>>>>>' and d['column_nmb'] != 'tab delimited']
+    ref_dicts = [d for d in data_model[0] if d['column_nmb'] != '>>>>>>>>>>']
+    file_types = [d['field_name'] for d in data_model[0] if d['column_nmb'] == 'tab delimited']
+    file_types.insert(0, data_model[1])
     complete_ref = {}
+
+    dictionary = {}
+    n = 0
     for d in ref_dicts:
-        complete_ref[d['field_name_oracle']] = {'data_type': d['data_type'], 'data_status': d['data_status']}
+        if d['field_name'] in file_types:
+            complete_ref[file_types[n]] = dictionary
+            n += 1
+            dictionary = {}
+        else:
+            dictionary[d['field_name_oracle']] = {'data_type': d['data_type'], 'data_status': d['data_status']}
     return complete_ref
 
 
@@ -40,8 +60,8 @@ def read_upload(up_file):
     take a file that should be ready for upload
     using the data model, check that all required columns are full,
     and that all numeric data is in fact numeric.
-    print out any validation problems
-    return True if there were no problems, otherwise return false
+    print out warnings for any validation problems
+    return True if there were no problems, otherwise return False
     """
     print "-I- Running validation for your upload file"
     f = open(up_file)
@@ -54,7 +74,7 @@ def read_upload(up_file):
     invalid_col_names = {}
     missing_file_type = False
     data_model = get_data_model()
-    reqd_file_types = ['magic_measurements', 'er_specimens']
+    reqd_file_types = ['er_locations']
     provided_file_types = set()
     if not data_model:
         return False
@@ -64,25 +84,26 @@ def read_upload(up_file):
                 provided_file_types.add(v)
                 continue
             file_type = dictionary['file_type']
+            specific_data_model = data_model[file_type]
 
             # check if column header is in the data model
-            invalid_col_name = validate_for_recognized_column(k, v, data_model)
+            invalid_col_name = validate_for_recognized_column(k, v, specific_data_model)
             if invalid_col_name:
                 if file_type not in invalid_col_names.keys():
                     invalid_col_names[file_type] = set()
                 invalid_col_names[file_type].add(invalid_col_name)
-                # skip to next item, as addition validations won't work (key is not in the data model)
+                # skip to next item, as additional validations won't work (key is not in the data model)
                 continue 
             
             # make a list of missing, required data
-            missing_item = validate_for_presence(k, v, data_model) 
+            missing_item = validate_for_presence(k, v, specific_data_model) 
             if missing_item:
                 if file_type not in missing_data.keys():
                     missing_data[file_type] = set()
                 missing_data[file_type].add(missing_item)
 
             # make a list of data that should be numeric, but isn't
-            number_fail = validate_for_numericality(k, v, data_model)
+            number_fail = validate_for_numericality(k, v, specific_data_model)
             if number_fail:
                 if file_type not in number_scramble.keys():
                     number_scramble[file_type] = set()
@@ -92,7 +113,7 @@ def read_upload(up_file):
         print "-W- In your {} file, you are using the following unrecognized columns: {}".format(file_type, ', '.join(invalid_names))
 
     for file_type, wrong_cols in number_scramble.items():
-        print "-W- In your {} file, you must provide a valid number, in the following columns: {}".format(file_type, ', '.join(wrong_cols))
+        print "-W- In your {} file, you must provide only valid numbers, in the following columns: {}".format(file_type, ', '.join(wrong_cols))
 
     for file_type, empty_cols in missing_data.items():
         print "-W- In your {} file, you are missing data in the following required columns: {}".format(file_type, ', '.join(empty_cols))
@@ -108,7 +129,6 @@ def read_upload(up_file):
     else:
         print "-I- validation was successful"
         return True
-    
     
 
 def split_lines(lines):
