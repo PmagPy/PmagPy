1635d9140391656514c09c4fe39a4557bfe61347
Lori J <mintblue87@gmail.com>
2015-01-23 15:22:47 -0800

    stop using pmagpyrc; remove matplotlib.use("TKAgg")

--- pmagpyrc.py
+++ pmagpyrc.py
@@ -1,9 +1,10 @@
+# deprecated to keep name-spacing simple
 import matplotlib
 import numpy
 from matplotlib import rc
 import os
 #if "OSTYPE" in os.environ.keys() and "darwin" in os.environ['OSTYPE']:
 #    matplotlib.use("TkAgg")
-#matplotlib.use("TkAgg")
+matplotlib.use("TkAgg")
 isServer = False
 verbose = True
