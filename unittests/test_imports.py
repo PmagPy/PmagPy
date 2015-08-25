dd5f58d960ac128877ee52e45e203b04f839d0fc
Lori J <mintblue87@gmail.com>
2015-08-25 12:52:27 -0600

    hopefully fixed BGC_magic remote testing problem

--- unittests/test_imports.py
+++ unittests/test_imports.py
@@ -210,7 +210,6 @@ class TestIODP_dscr_magic(unittest.TestCase):
         options['input_dir_path'] = dir_path
         options['csv_file'] = 'srmdiscrete-XXX-UTEST-A.csv'
         program_ran, outfile = IODP_dscr_magic.main(False, **options)
-        print 'outfile!', outfile
         self.assertTrue(program_ran)
         self.assertEqual(outfile, os.path.join('.', 'magic_measurements.txt'))
 
@@ -385,19 +384,6 @@ class TestBGC_magic(unittest.TestCase):
 
     def tearDown(self):
         filelist = ['96MT.05.01.magic', 'BC0-3A.magic', 'magic_measurements.txt', 'er_specimens.txt', 'er_samples.txt', 'er_sites.txt']
-        #directory = os.path.join(WD, 'Datafiles', 'Measurement_Import', 'BGC_magic')
-        #print 'directory', directory
-        print 'parent_dir', os.path.join(WD, 'Datafiles', 'Measurement_Import')
-        print "os.listdir(os.path.join(WD, 'Datafiles', 'Measurement_Import')", os.path.join(WD, 'Datafiles', 'Measurement_Import')
-        print '-'
-        print 'self.input_dir:', self.input_dir
-        print '-'
-        print 'os.path.isdir(self.input_dir):', os.path.isdir(self.input_dir)
-        print '-'
-        print 'os.listdir(self.input_dir):', os.listdir(self.input_dir)
-        print '-'
-        print 'os.listdir(os.path.join(WD, "Datafiles", "Measurement_import")):', os.listdir(os.path.join(WD, "Datafiles", "Measurement_import"))
-        print '-'
         pmag.remove_files(filelist, self.input_dir)
 
     def test_BGC_with_no_files(self):
@@ -406,9 +392,6 @@ class TestBGC_magic(unittest.TestCase):
         self.assertEqual(error_message, 'You must provide a BCG format file')
 
     def test_BGC_success(self):
-        print 'in test_BGC_success'
-        print 'self.input_dir', self.input_dir
-        print 'os.listdir(self.input_dir)', os.listdir(self.input_dir)
         options = {'input_dir_path': self.input_dir, 'mag_file': '96MT.05.01'}
         program_ran, outfile = BGC_magic.main(False, **options)
         self.assertTrue(program_ran)
