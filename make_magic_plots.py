f87f79b8757f7f92476f20187c6e8072f42b1974
Lisa Tauxe <ltauxe@ucsd.edu>
2015-12-01 19:35:50 -0800

    modified:   histplot.py => added -sav option 	modified:   make_magic_plots.py => added histograms of intensities

--- make_magic_plots.py
+++ make_magic_plots.py
@@ -46,43 +46,48 @@ def main():
             Srecs=pmag.get_dictitem(samps,'sample_azimuth','','F')# get all none blank sample orientations
             if len(Srecs)>0: 
                 crd='g'
-        if 'magic_measurements.txt' in filelist: # start with measurement data
-            print 'working on measurements data'
-            data,file_type=pmag.magic_read('magic_measurements.txt') # read in data
-            if loc == './': data=pmag.get_dictitem(data,'er_location_name','','T') # get all the blank location names from data file
-            # looking for  zeq_magic possibilities
-            AFZrecs=pmag.get_dictitem(data,'magic_method_codes','LT-AF-Z','has')# get all none blank method codes
-            TZrecs=pmag.get_dictitem(data,'magic_method_codes','LT-T-Z','has')# get all none blank method codes
-            MZrecs=pmag.get_dictitem(data,'magic_method_codes','LT-M-Z','has')# get all none blank method codes
-            Drecs=pmag.get_dictitem(data,'measurement_dec','','F') # get all dec measurements
-            Irecs=pmag.get_dictitem(data,'measurement_inc','','F') # get all dec measurements
-            Mkeys=['measurement_magnitude','measurement_magn_moment','measurement_magn_volume','measurement_magn_mass']
-            for key in Mkeys:
-                Mrecs=pmag.get_dictitem(data,key,'','F') # get intensity data
-                if len(Mrecs)>0:break
-            if len(AFZrecs)>0 or len(TZrecs)>0 or len(MZrecs)>0 and len(Drecs)>0 and len(Irecs)>0 and len(Mrecs)>0: # potential for stepwise demag curves 
-                print 'zeq_magic.py -fsp pmag_specimens.txt -sav -fmt '+fmt+' -crd '+crd
-                os.system('zeq_magic.py -sav -fmt '+fmt+' -crd '+crd )
-                #pass
-            # looking for  thellier_magic possibilities
-            if len(pmag.get_dictitem(data,'magic_method_codes','LP-PI-TRM','has'))>0:
-                print 'thellier_magic.py -fsp pmag_specimens.txt -sav -fmt '+fmt
-                os.system('thellier_magic.py -sav -fmt '+fmt)
-                #pass
-            # looking for hysteresis possibilities
-            if len(pmag.get_dictitem(data,'magic_method_codes','LP-HYS','has'))>0: # find hyst experiments
-                print 'quick_hyst.py -sav -fmt '+fmt
-                os.system('quick_hyst.py -sav -fmt '+fmt)
-                #pass
+#        if 'magic_measurements.txt' in filelist: # start with measurement data
+#            print 'working on measurements data'
+#            data,file_type=pmag.magic_read('magic_measurements.txt') # read in data
+#            if loc == './': data=pmag.get_dictitem(data,'er_location_name','','T') # get all the blank location names from data file
+#            # looking for  zeq_magic possibilities
+#            AFZrecs=pmag.get_dictitem(data,'magic_method_codes','LT-AF-Z','has')# get all none blank method codes
+#            TZrecs=pmag.get_dictitem(data,'magic_method_codes','LT-T-Z','has')# get all none blank method codes
+#            MZrecs=pmag.get_dictitem(data,'magic_method_codes','LT-M-Z','has')# get all none blank method codes
+#            Drecs=pmag.get_dictitem(data,'measurement_dec','','F') # get all dec measurements
+#            Irecs=pmag.get_dictitem(data,'measurement_inc','','F') # get all dec measurements
+#            Mkeys=['measurement_magnitude','measurement_magn_moment','measurement_magn_volume','measurement_magn_mass']
+#            for key in Mkeys:
+#                Mrecs=pmag.get_dictitem(data,key,'','F') # get intensity data
+#                if len(Mrecs)>0:break
+#            if len(AFZrecs)>0 or len(TZrecs)>0 or len(MZrecs)>0 and len(Drecs)>0 and len(Irecs)>0 and len(Mrecs)>0: # potential for stepwise demag curves 
+#                print 'zeq_magic.py -fsp pmag_specimens.txt -sav -fmt '+fmt+' -crd '+crd
+#                os.system('zeq_magic.py -sav -fmt '+fmt+' -crd '+crd )
+#                #pass
+#            # looking for  thellier_magic possibilities
+#            if len(pmag.get_dictitem(data,'magic_method_codes','LP-PI-TRM','has'))>0:
+#                print 'thellier_magic.py -fsp pmag_specimens.txt -sav -fmt '+fmt
+#                os.system('thellier_magic.py -sav -fmt '+fmt)
+#                #pass
+#            # looking for hysteresis possibilities
+#            if len(pmag.get_dictitem(data,'magic_method_codes','LP-HYS','has'))>0: # find hyst experiments
+#                print 'quick_hyst.py -sav -fmt '+fmt
+#                os.system('quick_hyst.py -sav -fmt '+fmt)
+#                #pass
         if 'pmag_results.txt' in filelist: # start with measurement data
-            print 'working on pmag_results'
             data,file_type=pmag.magic_read('pmag_results.txt') # read in data
+            print 'number of datapoints: ',len(data) 
             if loc == './': data=pmag.get_dictitem(data,'er_location_names',':','has') # get all the concatenated location names from data file
-            SiteDIs=pmag.get_dictitem(data,'average_dec','','F') # find decs
-            SiteDIs=pmag.get_dictitem(SiteDIs,'average_inc','','F') # find decs and incs
-            SiteDIs=pmag.get_dictitem(SiteDIs,'data_type','i','T') # only individual results - not poles
+            print 'number of datapoints: ',len(data) ,loc
+            print 'working on pmag_results directions'
+            SiteDIs=pmag.get_dictitem(data,'average_dec',"",'F') # find decs
+            print 'number of directions: ',len(SiteDIs) 
+            SiteDIs=pmag.get_dictitem(SiteDIs,'average_inc',"",'F') # find decs and incs
+            print 'number of directions: ',len(SiteDIs) 
+            SiteDIs=pmag.get_dictitem(SiteDIs,'data_type','i','has') # only individual results - not poles
+            print 'number of directions: ',len(SiteDIs) 
             SiteDIs_t=pmag.get_dictitem(SiteDIs,'tilt_correction','100','T')# tilt corrected coordinates
-            
+            print 'number of directions: ',len(SiteDIs) 
             if len(SiteDIs_t)>0:
                 print 'eqarea_magic.py -sav -crd t -fmt '+fmt
                 os.system('eqarea_magic.py -sav -crd t -fmt '+fmt)
@@ -104,6 +109,17 @@ def main():
                         if len(SiteDIs_x)>0:
                             print 'eqarea_magic.py -sav -fmt '+fmt
                             os.system('eqarea_magic.py -sav -fmt '+fmt)
+            print 'working on pmag_results intensities'
+        
+            os.system('magic_select.py -f pmag_results.txt -key data_type i T -F tmp.txt')
+            os.system('magic_select.py -f tmp.txt -key average_int 0. has -F tmp1.txt')
+            os.system("grab_magic_key.py -f tmp1.txt -key average_int | awk '{print $1*1e6}' >tmp2.txt")
+            data,file_type=pmag.magic_read('tmp1.txt') # read in data
+            locations=pmag.get_dictkey(data,'er_location_names',"")
+            histfile='LO:_'+locations[0]+'_intensities_histogram:_.'+fmt
+            os.system("histplot.py -b 1 -xlab 'Intensity (uT)' -sav -f tmp2.txt -F " +histfile)
+            print "histplot.py -b 1 -xlab 'Intensity (uT)' -sav -f tmp2.txt -F " +histfile
+            os.system('rm tmp*.txt')
         if 'rmag_hysteresis.txt' in filelist: # start with measurement data
             print 'working on rmag_hysteresis'
             data,file_type=pmag.magic_read('rmag_hysteresis.txt') # read in data
