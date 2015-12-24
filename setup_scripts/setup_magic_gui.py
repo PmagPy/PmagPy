abe88c596a2c208d90ddf5156af9a7fb6850a07c
Lori J <mintblue87@gmail.com>
2015-12-23 16:44:46 -0800

    folder fixes -rename setup script -ignore .eggs folder (contains unneeded py2app output)

--- setup_scripts/setup_magic_gui.py
+++ setup_scripts/setup_magic_gui.py
@@ -9,7 +9,7 @@ from setuptools import setup
 import os
 directory = os.getcwd()
 
-APP = ['make_magic.py']
+APP = ['magic_gui.py']
 DATA_FILES = [('', ['help_files', 'data_model', 'documentation', 'images'])]
 OPTIONS = {'argv_emulation': False,
            'iconfile': os.path.join(directory, 'images', 'text_x_xslfo.icns')}
