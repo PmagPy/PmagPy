0a5e584be7de8d21f46a3f205b1d8c740cb27211
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2016-01-27 15:10:08 -0800

    change n to nT in igrf_print

--- pmagpy/ipmag.py
+++ pmagpy/ipmag.py
@@ -32,7 +32,7 @@ def igrf(input_list):
 def igrf_print(igrf_array):
     print "Declination: %0.3f"%(igrf_array[0])
     print "Inclination: %0.3f"%(igrf_array[1])
-    print "Intensity: %0.3f n"%(igrf_array[0])
+    print "Intensity: %0.3f nT"%(igrf_array[0])
 
 
 def fisher_mean(dec=None, inc=None, di_block=None):
