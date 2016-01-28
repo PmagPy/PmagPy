5b5b53cf07f974c6271fdcac46f25f2d4ae08f4f
Lori J <mintblue87@gmail.com>
2016-01-28 12:55:42 -0800

    improve automation scripts

--- make_guis.sh
+++ make_guis.sh
@@ -6,9 +6,25 @@
 # commits them to PmagPy-Standalone
 # pushes the new commit to Github
 
+pyv="$(which python)"
+echo $pyv
+if [ "$pyv" = "/usr/local/bin/python" ]; then
+    echo 'Using correct Python distribution'
+else
+    echo "-W- Using incorrect Python distribution (this can't be done with Canopy)"
+    echo "-W- Please try again with a non-Canopy distribution"
+    exit
+fi
+
 new="$1"
-echo $new
-my_commit="update: $new"
+
+if [ -z "$new" ]; then
+    echo "-W- You need to provide a commit name"
+    echo 'Correct usage is: make_guis.sh "new commit name"'
+    exit
+fi
+
+my_commit="$new"
 echo $my_commit
 
 echo "starting make_guis script"
@@ -34,6 +50,11 @@ rm -rf ../PmagPy-Standalone/magic_gui.app
 echo "deleted old Pmag GUI"
 mv dist/magic_gui.app ../PmagPy-Standalone
 echo "moved magic_gui.app to PmagPy-Standalone"
+
+echo "clean up"
+rm setup_pmag_gui.py
+rm setup_magic_gui.py
+
 cd ../PmagPy-Standalone
 
 echo 'adding git stuff'
@@ -41,10 +62,6 @@ git add .
 echo 'adding .'
 git commit -m "$my_commit"
 echo 'committed'
-git push
 
-echo "clean up"
-rm setup_pmag_gui.py
-rm setup_magic_gui.py
 
-echo "Done!  Changes pushed to PmagPy-Standalone-OSX"
+echo "Done!  Changes have been committed to PmagPy-Standalone-OSX"
