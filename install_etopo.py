bfb47c7fffafa197f558bad7b8beee803c398c97
Lisa Tauxe <ltauxe@ucsd.edu>
2016-02-24 17:03:27 -0800

    modified:   data_files/_PmagPy.ipynb 	modified:   data_files/di_rot/fishrot.out 	modified:   data_files/tk03/tk03.out 	modified:   data_files/uniform/uniform.out 	deleted:    dipole_plat/dipole_plat_example.dat 	modified:   install_etopo.py => modified so will work without PYTHONPATH

--- install_etopo.py
+++ install_etopo.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 import os
+from os import path
 from mpl_toolkits.basemap import basemap_datadir
-print 'installing etopo20 files in the basemap data directory'
-print basemap_datadir
-p=os.environ['PYTHONPATH'].strip(':')
-os.system('cp '+p+'/data_files/etopo20/etopo20* ' + basemap_datadir)
+from pmagpy import check_updates
+pmag_dir = check_updates.get_pmag_dir()
+print "installing etopo20 files from ", path.join(pmag_dir, 'data_files', 'etopo20', 'etopo20*')
+print "to the basemap data directory: ",basemap_datadir
+command='cp ' + path.join(pmag_dir, 'data_files', 'etopo20', 'etopo20*')  + " "+ basemap_datadir
+os.system(command)
