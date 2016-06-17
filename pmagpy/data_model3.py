fa1ed1f30d6ccc5a2577acf48f19dad7cb593d7c
Lori J <mintblue87@gmail.com>
2016-06-17 12:36:31 -0600

    move data_model module to more appropriate location

---
+++ pmagpy/data_model3.py
@@ -0,0 +1,64 @@
+import os
+import json
+from pandas import DataFrame
+
+
+
+class DataModel(object):
+
+    def __init__(self):
+        self.dm = self.get_data_model()
+    
+    def download_data_model(self):
+        model_file = os.path.join('3_0', 'MagIC Data Model v3.0 - unpublished.json')
+        f = open(model_file, 'r')
+        string = '\n'.join(f.readlines())
+        raw = json.loads(unicode(string, errors='ignore'))
+        full = DataFrame(raw)
+        return full
+
+    def parse_data_model(self, full_df):
+        data_model = {}
+        levels = ['specimens', 'samples', 'sites', 'locations']
+        for level in levels:
+            df = DataFrame(full_df['tables'][level]['columns'])
+            data_model[level] = df.transpose()
+        return data_model
+
+
+    def get_data_model(self):
+        full_df = self.download_data_model()
+        print 'got full_df'
+        parsed_df = self.parse_data_model(full_df)
+        print 'got parsed_df'
+        return parsed_df
+
+    def get_groups(self, table_name):
+        """
+        Return list of all groups for a particular data type
+        """
+        df = self[table_name]
+        return list(df['group'].unique())
+
+
+    def get_headers(self, table_name, group_name):
+        """
+        Return a list of all headers for a given group
+        """
+        # get all headers of a particular group
+        df = DATA_MODEL[table_name]
+        cond = df['group'] == group_name
+        return df[cond].index
+
+
+
+DATA_MODEL = DataModel()
+#
+
+
+
+
+if __name__ == "__main__":
+    dm = DataModel()
+
+
