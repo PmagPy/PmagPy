cb0ee45b6696631c16da73497bcaf48ac37cd0b8
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2016-10-19 15:29:40 -0700

    fix __init__.py file in /pmagpy/mappings

--- pmagpy/mapping/__init__.py
+++ pmagpy/mapping/__init__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# __init__.py: -*- Python -*-  DESCRIPTIVE TEXT.
-# 
-#  Copyright (c) 2014 Lori Jonestrask
-#  Author: Lori Jonestrask (mintblue87@gmail.com) .
+import map_magic
+
+__all__ = [map_magic]
