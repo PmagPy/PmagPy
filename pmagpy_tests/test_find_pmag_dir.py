73adec9e7f12d66abe0b6637139b6e882c9c956d
Lori J <mintblue87@gmail.com>
2016-10-26 13:46:26 -0700

    add tests for find_pmag_dir

---
+++ pmagpy_tests/test_find_pmag_dir.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import unittest
+import os
+import sys
+import matplotlib
+from pmagpy import pmag
+from pmagpy import ipmag
+from pmagpy import find_pmag_dir
+WD = os.getcwd()
+
+
+class TestFindPmagDir(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        os.chdir(WD)
+
+    def test_local(self):
+        dir1 = find_pmag_dir.get_pmag_dir()
+        os.chdir('..')
+        os.chdir('..')
+        dir2 = find_pmag_dir.get_pmag_dir()
+        os.chdir(dir2)
+        dir3 = find_pmag_dir.get_pmag_dir()
+        self.assertEqual(dir1, dir2)
+        self.assertEqual(dir2, dir3)
+        if "-pip" not in sys.argv:
+            self.assertIn('PmagPy', dir1)
