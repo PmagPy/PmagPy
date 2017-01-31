0d1bfefecc4fe0cc83868fa9f9f5f2d985333488
Lori J <mintblue87@gmail.com>
2017-01-31 12:44:38 -0800

    cleanup in map_magic.  make main mappings more thorough (still incomplete — no merging or splitting records, etc.).  store mappings in maps.py and add function to map_magic for re-generating maps.py from data model if necessary.  add tests for map_magic.

--- pmagpy_tests/__init__.py
+++ pmagpy_tests/__init__.py
@@ -19,8 +19,10 @@ import test_demag_gui
 import test_magic_gui2
 import test_new_builder
 import test_find_pmag_dir
+import test_map_magic
 
 __all__ = [test_builder, test_er_magic_dialogs, test_imports, test_ipmag,
            test_magic_gui, test_pmag, test_pmag_gui, test_thellier_gui,
            test_validations, test_programs, test_demag_gui,
-           test_magic_gui2, test_new_builder, test_find_pmag_dir]
+           test_magic_gui2, test_new_builder, test_find_pmag_dir,
+           test_map_magic]
