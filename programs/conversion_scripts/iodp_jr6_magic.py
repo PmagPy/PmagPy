3bef8efe1038220474ea7f54540903a10f5a0496
Caoimhinmg <caoimhinmg@gmail.com>
2017-02-21 14:44:31 -0800

    created a 3.0 native iodp jr6 script as per #135

--- programs/conversion_scripts/iodp_jr6_magic.py
+++ programs/conversion_scripts/iodp_jr6_magic.py
@@ -11,10 +11,20 @@ SYNTAX
 
 OPTIONS
     -h: prints the help message and quits.
-    -f FILE: specify  input file, or
-    -F FILE: specify output file, default is magic_measurements.txt
-    -loc HOLE : specify hole name (U1456A)
+    -ID: directory for input file if not included in -f flag
+    -f FILE: specify input .csv file, default is all in directory
+    -WD: directory to output files to (default : current directory)
+    -F FILE: specify output  measurements file, default is measurements.txt
+    -Fsp FILE: specify output specimens.txt file, default is specimens.txt
+    -Fsa FILE: specify output samples.txt file, default is samples.txt 
+    -Fsi FILE: specify output sites.txt file, default is sites.txt
+    -Flo FILE: specify output locations.txt file, default is locations.txt
+    -lat LAT: latitude of site (also used as bounding latitude for location)
+    -lon LON: longitude of site (also used as bounding longitude for location)
+    -exp EXPPEDITION: specify expedition name (i.e. 312)
+    -site HOLE: specify hole name (i.e. U1456A)
     -A: don't average replicate measurements
+    -v NUM: volume in cm^3, will be used if there is no volume in the input data (default : 15.625 cm^3 or a 2.5 cm cube)
 
 INPUT
     JR6 .jr6 format file
@@ -27,43 +37,16 @@ from pandas import DataFrame
 
 def fix_separation(filename, new_filename):
     old_file = open(filename, 'rU')
-    data = old_file.readlines()
-    new_data = []
-    for line in data:
-        new_line = line.replace('-', ' -')
-        new_line = new_line.replace('  ', ' ')
-        new_data.append(new_line)
     new_file = open(new_filename, 'w')
-    for s in new_data:
-        new_file.write(s)
+    data = old_file.readlines()
+    for line in data:
+        ldata=line.split()
+        if len(ldata[0])>10: ldata.insert(1,ldata[0][10:]); ldata[0]=ldata[0][:10]
+        ldata=[ldata[0]]+[d.replace('-', ' -') for d in ldata[1:]]
+        new_line = ' '.join(ldata)+'\n'
+        new_file.write(new_line)
     old_file.close()
     new_file.close()
-    return new_filename
-
-def old_fix_separation(filename, new_filename):
-    old_file = open(filename, 'rU')
-    data = old_file.readlines()
-    new_data = []
-    for line in data:
-        new_line = []
-        for i in line.split():
-            if '-' in i[1:]:
-                lead_char = '-' if i[0] == '-' else ''
-                if lead_char:
-                    v = i[1:].split('-')
-                else:
-                    v = i.split('-')
-                new_line.append(lead_char + v[0])
-                new_line.append('-' + v[1])
-            else:
-                new_line.append(i)
-        new_line = (' '.join(new_line)) + '\n'
-        new_data.append(new_line)
-    new_file = open(new_filename, 'w')
-    for s in new_data:
-        new_file.write(s)
-    new_file.close()
-    old_file.close()
     return new_filename
 
 def main(**kwargs):
@@ -72,36 +55,22 @@ def main(**kwargs):
     demag="N"
     version_num=pmag.get_version()
 
-    dir_path = kwsys.argv.get('dir_path', '.')
-    input_dir_path = kwsys.argv.get('input_dir_path', dir_path)
+    dir_path = kwargs.get('dir_path', '.')
+    input_dir_path = kwargs.get('input_dir_path', dir_path)
     output_dir_path = dir_path
-    meas_file = kwsys.argv.get('meas_file', 'magic_measurements.txt')
-    mag_file = kwsys.argv.get('mag_file', '')
-    specnum = kwsys.argv.get('specnum', 1)
-    samp_con = kwsys.argv.get('samp_con', '1')
-    location = kwsys.argv.get('location', 'unknown')
-    noave = kwsys.argv.get('noave', False) # default means DO average
-    meth_code = kwsys.argv.get('meth_code', "LP-NO")
-    volume = kwsys.argv.get('volume', 2.5**3)#default volume is a 2.5cm cube
-
-    # format variables
-    if specnum!=0:
-        specnum=-specnum
-    if "4" in samp_con:
-        if "-" not in samp_con:
-            print "option [4] must be in form 4-Z where Z is an integer"
-            return False, "option [4] must be in form 4-Z where Z is an integer"
-        else:
-            Z=int(samp_con.split("-")[1])
-            samp_con="4"
-    if "7" in samp_con:
-        if "-" not in samp_con:
-            print "option [7] must be in form 7-Z where Z is an integer"
-            return False, "option [7] must be in form 7-Z where Z is an integer"
-        else:
-            Z=int(samp_con.split("-")[1])
-            samp_con="7"
-    else: Z=1
+    meas_file = kwargs.get('meas_file', 'measurements.txt')
+    spec_file = kwargs.get('spec_file', 'specimens.txt')
+    samp_file = kwargs.get('samp_file', 'samples.txt')
+    site_file = kwargs.get('site_file', 'sites.txt')
+    loc_file = kwargs.get('loc_file', 'locations.txt')
+    mag_file = kwargs.get('mag_file', '')
+    site = kwargs.get('site', 'unknown')
+    expedition = kwargs.get('expedition', 'unknown')
+    lat = kwargs.get('lat', '')
+    lon = kwargs.get('lon', '')
+    noave = kwargs.get('noave', False) # default means DO average
+    meth_code = kwargs.get('meth_code', "LP-NO")
+    volume = kwargs.get('volume', 2.5**3)*1e-6#default volume is a 2.5cm cube
 
     meth_code=meth_code+":FS-C-DRILL-IODP:SP-SS-C:SO-V"
     meth_code=meth_code.strip(":")
@@ -115,39 +84,41 @@ def main(**kwargs):
     # parse data
     temp = os.path.join(output_dir_path, 'temp.txt')
     fix_separation(mag_file, temp)
-    samples,filetype = pmag.magic_read(samp_file)
     lines = open(temp, 'rU').readlines()
-    os.remove(temp)
+    try: os.remove(temp)
+    except OSError: print("problem with temp file")
+    citation="This Study"
     MeasRecs,SpecRecs,SampRecs,SiteRecs,LocRecs=[],[],[],[],[]
     for line in lines:
         MeasRec,SpecRec,SampRec,SiteRec,LocRec={},{},{},{},{}
         line = line.split()
-        spec_text_id = line[0].split('_')[1]
+        spec_text_id = line[0]
+        specimen = spec_text_id
+        for dem in ['-','_']:
+            if dem in spec_text_id:
+                sample=dem.join(spec_text_id.split(dem)[:-1]); break
+        location = expedition
 
         if specimen!="" and specimen not in map(lambda x: x['specimen'] if 'specimen' in x.keys() else "", SpecRecs):
             SpecRec['specimen'] = specimen
             SpecRec['sample'] = sample
+            SpecRec['volume'] = volume
             SpecRec['citations']=citation
-            SpecRec['specimen_alternatives']=InRec[text_id]
             SpecRecs.append(SpecRec)
         if sample!="" and sample not in map(lambda x: x['sample'] if 'sample' in x.keys() else "", SampRecs):
             SampRec['sample'] = sample
             SampRec['site'] = site
             SampRec['citations']=citation
-            SampRec['azimuth']='0'
-            SampRec['dip']='0'
-            SampRec['core_depth']=InRec[depth_key]
-            if comp_depth_key!='':
-                SampRec['composite_depth']=InRec[comp_depth_key]
-            if "SHLF" not in InRec[text_id]:
-                SampRec['method_codes']='FS-C-DRILL-IODP:SP-SS-C:SO-V'
-            else:
-                SampRec['method_codes']='FS-C-DRILL-IODP:SO-V'
+            SampRec['azimuth']=line[6]
+            SampRec['dip']=line[7]
+            SampRec['bed_dip_direction']=line[8]
+            SampRec['bed_dip']=line[9]
+            SampRec['method_codes']=meth_code
             SampRecs.append(SampRec)
         if site!="" and site not in map(lambda x: x['site'] if 'site' in x.keys() else "", SiteRecs):
             SiteRec['site'] = site
             SiteRec['location'] = location
-            SiteRec['citations']=citation
+            SiteRec['citations']= citation
             SiteRec['lat'] = lat
             SiteRec['lon'] = lon
             SiteRecs.append(SiteRec)
@@ -161,17 +132,16 @@ def main(**kwargs):
             LocRec['lon_w'] = lon
             LocRecs.append(LocRec)
 
-        MeasRec['er_sample_name']=MeasRec['er_specimen_name']
-        MeasRec["er_citation_names"]="This study"
-        MeasRec['magic_software_packages']=version_num
-        MeasRec["treatment_temp"]='%8.3e' % (273) # room temp in kelvin
-        MeasRec["measurement_temp"]='%8.3e' % (273) # room temp in kelvin
-        MeasRec["measurement_flag"]='g'
-        MeasRec["measurement_standard"]='u'
-        MeasRec["measurement_number"]='1'
-        MeasRec["treatment_ac_field"]='0'
+        MeasRec['specimen']=specimen
+        MeasRec["citations"]=citation
+        MeasRec['software_packages']=version_num
+        MeasRec["treat_temp"]='%8.3e' % (273) # room temp in kelvin
+        MeasRec["meas_temp"]='%8.3e' % (273) # room temp in kelvin
+        MeasRec["quality"]='g'
+        MeasRec["standard"]='u'
+        MeasRec["number"]='1'
+        MeasRec["treat_ac_field"]='0'
 
-        volume=float(SampRecs[0]['sample_volume'])
         x = float(line[4])
         y = float(line[3])
         negz = float(line[2])
@@ -181,10 +151,10 @@ def main(**kwargs):
         magn_volume = direction[2] * (10.0**expon)
         moment = magn_volume * volume
 
-        MeasRec["measurement_magn_moment"]=str(moment)
-        MeasRec["measurement_magn_volume"]=str(magn_volume)#str(direction[2] * (10.0 ** expon))
-        MeasRec["measurement_dec"]='%7.1f'%(direction[0])
-        MeasRec["measurement_inc"]='%7.1f'%(direction[1])
+        MeasRec["magn_moment"]=str(moment)
+        MeasRec["magn_volume"]=str(magn_volume)#str(direction[2] * (10.0 ** expon))
+        MeasRec["dir_dec"]='%7.1f'%(direction[0])
+        MeasRec["dir_inc"]='%7.1f'%(direction[1])
 
         step = line[1]
         if step == 'NRM':
@@ -192,61 +162,107 @@ def main(**kwargs):
         elif step[0:2] == 'AD':
             meas_type="LT-AF-Z"
             treat=float(step[2:])
-            MeasRec["treatment_ac_field"]='%8.3e' %(treat*1e-3) # convert from mT to tesla
+            MeasRec["treat_ac_field"]='%8.3e' %(treat*1e-3) # convert from mT to tesla
         elif step[0:2] == 'TD':
             meas_type="LT-T-Z"
             treat=float(step[2:])
-            MeasRec["treatment_temp"]='%8.3e' % (treat+273.) # temp in kelvin
+            MeasRec["treat_temp"]='%8.3e'%(treat+273.) # temp in kelvin
         elif step[0:3]=='ARM': #
             meas_type="LT-AF-I"
             treat=float(row['step'][3:])
-            MeasRec["treatment_ac_field"]='%8.3e' %(treat*1e-3) # convert from mT to tesla
-            MeasRec["treatment_dc_field"]='%8.3e' %(50e-6) # assume 50uT DC field
+            MeasRec["treat_ac_field"]='%8.3e' %(treat*1e-3) # convert from mT to tesla
+            MeasRec["treat_dc_field"]='%8.3e' %(50e-6) # assume 50uT DC field
             MeasRec["measurement_description"]='Assumed DC field - actual unknown'
+        elif step[0] == 'A':
+            meas_type="LT-AF-Z"
+            treat=float(step[1:])
+            MeasRec["treat_ac_field"]='%8.3e' %(treat*1e-3) # convert from mT to tesla
+        elif step[0] == 'T':
+            meas_type="LT-T-Z"
+            treat=float(step[1:])
+            MeasRec["treat_temp"]='%8.3e' % (treat+273.) # temp in kelvin
         elif step[0:3]=='IRM': #
             meas_type="LT-IRM"
             treat=float(step[3:])
-            MeasRec["treatment_dc_field"]='%8.3e' %(treat*1e-3) # convert from mT to tesla
+            MeasRec["treat_dc_field"]='%8.3e' %(treat*1e-3) # convert from mT to tesla
         else:
             print 'unknown treatment type for ',row
             return False, 'unknown treatment type for ',row
 
-        MeasRec['magic_method_codes']=meas_type
+        MeasRec['method_codes']=meas_type
         MeasRecs.append(MeasRec.copy())
 
-    MagOuts=pmag.measurements_methods(MeasRecs,noave)
-    file_created, error_message = pmag.magic_write(meas_file,MagOuts,'magic_measurements')
-    if file_created:
-        return True, meas_file
-    else:
-        return False, 'Results not written to file'
+    MagOuts=pmag.measurements_methods3(MeasRecs,noave)
+
+    con = nb.Contribution(output_dir_path,read_tables=[])
+
+    con.add_empty_magic_table('specimens')
+    con.add_empty_magic_table('samples')
+    con.add_empty_magic_table('sites')
+    con.add_empty_magic_table('locations')
+    con.add_empty_magic_table('measurements')
+
+    con.tables['specimens'].df = DataFrame(SpecRecs)
+    con.tables['samples'].df = DataFrame(SampRecs)
+    con.tables['sites'].df = DataFrame(SiteRecs)
+    con.tables['locations'].df = DataFrame(LocRecs)
+    con.tables['measurements'].df = DataFrame(MagOuts)
+
+    con.tables['specimens'].write_magic_file(custom_name=spec_file)
+    con.tables['samples'].write_magic_file(custom_name=samp_file)
+    con.tables['sites'].write_magic_file(custom_name=site_file)
+    con.tables['locations'].write_magic_file(custom_name=loc_file)
+    con.tables['measurements'].write_magic_file(custom_name=meas_file)
+
+    return (True, meas_file)
 
 
 if __name__ == '__main__':
-    kwsys.argv={}
+    kwargs={}
     # get command line arguments
     if '-WD' in sys.argv:
         ind = sys.argv.index('-WD')
-        kwsys.argv['dir_path']=sys.argv[ind+1]
+        kwargs['dir_path']=sys.argv[ind+1]
     if '-ID' in sys.argv:
         ind = sys.argv.index('-ID')
-        kwsys.argv['input_dir_path'] = sys.argv[ind+1]
+        kwargs['input_dir_path'] = sys.argv[ind+1]
     if "-h" in sys.argv:
         help(__name__)
         sys.exit()
     if '-F' in sys.argv:
         ind=sys.argv.index("-F")
-        kwsys.argv['meas_file'] = sys.argv[ind+1]
+        kwargs['meas_file']=sys.argv[ind+1]
+    if '-Fsp' in sys.argv:
+        ind=sys.argv.index("-Fsp")
+        kwargs['spec_file'] = sys.argv[ind+1]
+    if '-Fsa' in sys.argv:
+        ind=sys.argv.index("-Fsa")
+        kwargs['samp_file'] = sys.argv[ind+1]
+    if '-Fsi' in sys.argv:
+        ind=sys.argv.index("-Fsi")
+        kwargs['site_file']=sys.argv[ind+1]
+    if '-Flo' in sys.argv: # Kevin addition
+        ind=sys.argv.index("-Flo")
+        kwargs['loc_file']=sys.argv[ind+1]
     if '-f' in sys.argv:
         ind = sys.argv.index("-f")
-        kwsys.argv['mag_file']= sys.argv[ind+1]
-    if "-loc" in sys.argv:
-        ind=sys.argv.index("-loc")
-        kwsys.argv['location']=sys.argv[ind+1]
+        kwargs['mag_file']= sys.argv[ind+1]
+    if "-exp" in sys.argv:
+        ind=sys.argv.index("-exp")
+        kwargs['expedition']=sys.argv[ind+1]
+    if "-site" in sys.argv:
+        ind=sys.argv.index("-site")
+        kwargs['site']=sys.argv[ind+1]
     if "-A" in sys.argv:
-        kwsys.argv['noave']=True
+        kwargs['noave']=True
+    if "-lat" in sys.argv:
+        ind = sys.argv.index("-lat")
+        kwargs['lat'] = sys.argv[ind+1]
+    if "-lon" in sys.argv:
+        ind = sys.argv.index("-lon")
+        kwargs['lon'] = sys.argv[ind+1]
     if "-v" in sys.argv:
         ind=sys.argv.index("-v")
-        kwsys.argv['volume']=sys.argv[ind+1]
+        kwargs['volume']=sys.argv[ind+1]
 
-    main(**kwsys.argv)
+    main(**kwargs)
