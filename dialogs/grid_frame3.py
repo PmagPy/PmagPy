cd23e8625f1041b237b7f376b1ffe93f96a61e70
Lori J <mintblue87@gmail.com>
2017-02-23 15:45:18 -0800

    fix all crazy spacing problems!

--- dialogs/grid_frame3.py
+++ dialogs/grid_frame3.py
@@ -149,26 +149,20 @@ class GridFrame(wx.Frame):  # class GridFrame(wx.ScrolledWindow):
                                          name='toggle_help_btn')
         self.Bind(wx.EVT_BUTTON, self.toggle_help, self.toggle_help_btn)
         # message
-        width = 600
-        height = 140
         self.help_msg_boxsizer = wx.StaticBoxSizer(wx.StaticBox(self.panel, -1, name='help_msg_boxsizer'), wx.VERTICAL)
         self.default_msg_text = 'Edit {} here.\nYou can add or remove both rows and columns, however required columns may not be deleted.\nControlled vocabularies are indicated by **, and will have drop-down-menus.\nSuggested vocabularies are indicated by ^^, and also have drop-down-menus.\nTo edit all values in a column, click the column header.\nYou can cut and paste a block of cells from an Excel-like file.\nJust click the top left cell and use command "v".'.format(self.grid_type)
         txt = ''
         if self.grid_type == 'locations':
             txt = '\n\nNote: you can fill in location start/end latitude/longitude here.\nHowever, if you add sites in step 2, the program will calculate those values automatically,\nbased on site latitudes/logitudes.\nThese values will be written to your upload file.'
-            height += 70
         if self.grid_type == 'samples':
             txt = "\n\nNote: you can fill in lithology, class, and type for each sample here.\nHowever, if the sample's class, lithology, and type are the same as its parent site,\nthose values will propagate down, and will be written to your sample file automatically."
-            height += 70
         if self.grid_type == 'specimens':
             txt = "\n\nNote: you can fill in lithology, class, and type for each specimen here.\nHowever, if the specimen's class, lithology, and type are the same as its parent sample,\nthose values will propagate down, and will be written to your specimen file automatically."
-            height += 70
         if self.grid_type == 'ages':
             txt = "\n\nNote: only ages for which you provide data will be written to your upload file."
-            height += 30
         self.default_msg_text += txt
         self.msg_text = wx.StaticText(self.panel, label=self.default_msg_text,
-                                      style=wx.TE_CENTER, name='msg text', size=(width, height))
+                                      style=wx.TE_CENTER, name='msg text')
         self.help_msg_boxsizer.Add(self.msg_text)
         self.help_msg_boxsizer.ShowItems(False)
 
@@ -232,18 +226,19 @@ class GridFrame(wx.Frame):  # class GridFrame(wx.ScrolledWindow):
         self.grid_box.Add(self.grid, 1, flag=wx.ALL|wx.EXPAND, border=5)
 
         # final layout, set size
-        self.main_sizer.Add(self.hbox, flag=wx.ALL|wx.ALIGN_CENTER|wx.SHAPED , border=20)
-        self.main_sizer.Add(self.toggle_help_btn, .5,
-                            flag=wx.BOTTOM|wx.ALIGN_CENTRE|wx.SHAPED,
+        self.main_sizer.Add(self.hbox, flag=wx.ALL|wx.ALIGN_CENTER,#|wx.SHAPED,
+                            border=20)
+        self.main_sizer.Add(self.toggle_help_btn, 0,
+                            flag=wx.BOTTOM|wx.ALIGN_CENTRE,#|wx.SHAPED,
                             border=5)
-        self.main_sizer.Add(self.help_msg_boxsizer, .5,
-                            flag=wx.BOTTOM|wx.ALIGN_CENTRE|wx.SHAPED,
+        self.main_sizer.Add(self.help_msg_boxsizer, 0,
+                            flag=wx.BOTTOM|wx.ALIGN_CENTRE,
                             border=10)
-        self.main_sizer.Add(self.toggle_codes_btn, .5,
-                            flag=wx.BOTTOM|wx.ALIGN_CENTRE|wx.SHAPED,
+        self.main_sizer.Add(self.toggle_codes_btn, 0,
+                            flag=wx.BOTTOM|wx.ALIGN_CENTRE,#|wx.SHAPED,
                             border=5)
-        self.main_sizer.Add(self.code_msg_boxsizer, .5,
-                            flag=wx.BOTTOM|wx.ALIGN_CENTRE|wx.SHAPED,
+        self.main_sizer.Add(self.code_msg_boxsizer, 0,
+                            flag=wx.BOTTOM|wx.ALIGN_CENTRE,#|wx.SHAPED,
                             border=5)
         self.main_sizer.Add(self.grid_box, 2, flag=wx.ALL|wx.ALIGN_CENTER|wx.EXPAND, border=10)
         self.panel.SetSizer(self.main_sizer)
