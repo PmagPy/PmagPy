dca344068dee9db4392f5eb45c0e21f23348cdcf
Lori J <mintblue87@gmail.com>
2017-03-01 15:51:54 -0800

    improve readability of plot names

--- programs/eqarea.py
+++ programs/eqarea.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import os
 import numpy
 import matplotlib
 if matplotlib.get_backend() != "TKAgg":
@@ -17,18 +18,18 @@ def main():
 
     INPUT FORMAT
        takes dec/inc as first two columns in space delimited file
-   
+
     SYNTAX
        eqarea.py [options]
 
     OPTIONS
-        -f FILE, specify file on command line 
+        -f FILE, specify file on command line
         -sav save figure and quit
         -fmt [svg,jpg,png,pdf] set figure format [default is svg]
         -s  SIZE specify symbol size - default is 20
         -Lsym  SHAPE  COLOR specify shape and color for lower hemisphere
         -Usym  SHAPE  COLOR specify shape and color for upper hemisphere
-          shapes:  's': square,'o': circle,'^,>,v,<': [up,right,down,left] triangle, 'd': diamond, 
+          shapes:  's': square,'o': circle,'^,>,v,<': [up,right,down,left] triangle, 'd': diamond,
                    'p': pentagram, 'h': hexagon, '8': octagon, '+': plus, 'x': cross
           colors:  [b]lue,[g]reen,[r]ed,[c]yan,[m]agenta,[y]ellow,blac[k],[w]hite
 
@@ -43,7 +44,7 @@ def main():
     if '-sav' in sys.argv: plot=1
     if '-fmt'  in sys.argv:
         ind=sys.argv.index('-fmt')
-        fmt=sys.argv[ind+1] 
+        fmt=sys.argv[ind+1]
     if '-s'  in sys.argv:
         ind=sys.argv.index('-s')
         sym['size']=int(sys.argv[ind+1])
@@ -51,26 +52,26 @@ def main():
         sym['size']=20
     if '-Lsym'  in sys.argv:
         ind=sys.argv.index('-Lsym')
-        sym['lower'][0]=sys.argv[ind+1] 
-        sym['lower'][1]=sys.argv[ind+2] 
+        sym['lower'][0]=sys.argv[ind+1]
+        sym['lower'][1]=sys.argv[ind+2]
     if '-Usym'  in sys.argv:
         ind=sys.argv.index('-Usym')
-        sym['upper'][0]=sys.argv[ind+1] 
-        sym['upper'][1]=sys.argv[ind+2] 
+        sym['upper'][0]=sys.argv[ind+1]
+        sym['upper'][1]=sys.argv[ind+2]
     if '-f' in sys.argv: # ask for filename
         ind=sys.argv.index('-f')
-        file=sys.argv[ind+1]
-    else: 
+        fname=sys.argv[ind+1]
+    else:
         print main.__doc__
         print ' \n   -f option required'
         sys.exit() # graceful quit
-    DI=numpy.loadtxt(file)
+    DI=numpy.loadtxt(fname)
     EQ={'eq':1}
     pmagplotlib.plot_init(EQ['eq'],5,5)
     pmagplotlib.plotEQsym(EQ['eq'],DI,'Equal Area Plot',sym) # make plot
     if plot==0:pmagplotlib.drawFIGS(EQ) # make it visible
     for key in EQ.keys():
-        files[key]=key+'.'+fmt 
+        files[key]=key+'.'+fmt
     if pmagplotlib.isServer:
         black     = '#000000'
         purple    = '#800080'
@@ -79,7 +80,8 @@ def main():
         EQ = pmagplotlib.addBorders(EQ,titles,black,purple)
         pmagplotlib.saveP(EQ,files)
     elif plot==1:
-        files['eq']=file+'.'+fmt 
+        fname = os.path.split(fname)[1].split('.')[0]
+        files['eq']=fname+'_eq.'+fmt
         pmagplotlib.saveP(EQ,files)
     else:
         ans=raw_input(" S[a]ve to save plot, [q]uit without saving:  ")
