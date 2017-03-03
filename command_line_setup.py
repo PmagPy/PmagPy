PmagPy/PmagPy

commit 47f2e46d080de8fbbe9406aa40a800530faed4e7

pull out Demag GUI main into actual named main function (makes help work correctly for pip)

programs/demag_gui.py
@@ -6835,7 +6835,7 @@ def main(WD=None, standalone_app=True, parent=None, write_to_log_file=True, DM=N
         app.frame.Show()
         app.MainLoop()
 
-if __name__ == '__main__':
+def main():
     if "-h" in sys.argv:
         help(Demag_GUI)
         sys.exit()
@@ -6852,3 +6852,7 @@ if __name__ == '__main__':
         if len(sys.argv)>=dm_index+2:
             data_model = sys.argv[dm_index+1]
     main(WD=WD,write_to_log_file=write_to_log_file,DM=data_model)
+
+
+if __name__ == '__main__':
+    main()

commit 374b6b04eb7950099a342305faf00496534d130d

fix namespace error in Demag GUI introduced by 47f2e46

command_line_setup.py
@@ -137,7 +137,7 @@ setup(
             'gui_scripts': [
                     'magic_gui.py = programs.magic_gui:main',
                     'pmag_gui.py = programs.pmag_gui:main',
-                    'demag_gui.py = programs.demag_gui:main',
+                    'demag_gui.py = programs.demag_gui:do_main',
                     'thellier_gui.py = programs.thellier_gui:main',
                     'pmag_gui = programs.pmag_gui:main',
                     'magic_gui2.py = programs.magic_gui2:main',

programs/demag_gui.py
@@ -6835,7 +6835,7 @@ def main(WD=None, standalone_app=True, parent=None, write_to_log_file=True, DM=N
         app.frame.Show()
         app.MainLoop()
 
-def main():
+def do_main():
     if "-h" in sys.argv:
         help(Demag_GUI)
         sys.exit()
@@ -6855,4 +6855,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    do_main()