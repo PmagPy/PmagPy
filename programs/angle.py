6c75ab10d365107445c96c74919f14c81f3139fe
Lori J <mintblue87@gmail.com>
2017-04-17 12:56:27 -0700

    fix namespace error in angle.py

--- programs/angle.py
+++ programs/angle.py
@@ -11,24 +11,24 @@ def main():
     """
     NAME
         angle.py
-    
+
     DESCRIPTION
       calculates angle between two input directions D1,D2
-    
-    INPUT (COMMAND LINE ENTRY) 
+
+    INPUT (COMMAND LINE ENTRY)
            D1_dec D1_inc D1_dec D2_inc
     OUTPUT
            angle
-    
+
     SYNTAX
         angle.py [-h][-i] [command line options] [< filename]
-    
+
     OPTIONS
-        -h prints help and quits 
+        -h prints help and quits
         -i for interactive data entry
         -f FILE input filename
         -F FILE output filename (required if -F set)
-        Standard I/O 
+        Standard I/O
     """
     out=""
     if '-h' in sys.argv:
@@ -54,22 +54,22 @@ def main():
             except:
                 print("\nGood bye\n")
                 sys.exit()
-                 
+
             ang= pmag.angle(dir1,dir2)  # send dirs  to angle and spit out result
             print('%7.1f '%(ang))
     elif '-f' in sys.argv:
         ind=sys.argv.index('-f')
         file=sys.argv[ind+1]
-        input=numpy.loadtxt(file)
+        file_input=numpy.loadtxt(file)
     else:
-        input = numpy.loadtxt(sys.stdin.readlines(),dtype=numpy.float)  # read from standard input
-    if len(input.shape)>1: # list of directions
-        dir1,dir2=input[:,0:2],input[:,2:]
-    else: dir1,dir2=input[0:2],input[2:]
+        file_input = numpy.loadtxt(sys.stdin.readlines(),dtype=numpy.float)  # read from standard input
+    if len(file_input.shape)>1: # list of directions
+        dir1,dir2=file_input[:,0:2], file_input[:,2:]
+    else: dir1,dir2= file_input[0:2], file_input[2:]
     angs=pmag.angle(dir1,dir2)
     for ang in angs:   # read in the data (as string variable), line by line
         print('%7.1f'%(ang))
         if out!="":out.write('%7.1f \n'%(ang))
 
 if __name__ == "__main__":
-    main() 
+    main()
