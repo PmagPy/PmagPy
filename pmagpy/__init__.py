1626af5b86161f0b0e121b8b24eea2fc3b7ed3b5
Lori J <mintblue87@gmail.com>
2017-04-26 14:30:40 -0700

    Add Exception on import of pmagpy if using wrong Python version (should be impossible to install this version, but people are tricky….)

--- pmagpy/__init__.py
+++ pmagpy/__init__.py
@@ -1,3 +1,23 @@
+import sys
+if sys.version_info >= (3,):
+    raise Exception("""
+You are running Python {}.
+This version of pmagpy is only compatible with Python 2.
+Make sure you have pip >= 9.0 to avoid this kind of issue,
+as well as setuptools >= 24.2:
+
+ $ pip install pip setuptools --upgrade
+
+Then you should be able to download the correct version of pmagpy:
+
+ $ pip install pmagpy --upgrade
+
+If this still gives you an error, please report the issue:
+https://github.com/PmagPy/PmagPy/issues
+
+Thanks!
+
+""".format(sys.version))
 import pmag
 import ipmag
 import pmagplotlib
