3238ad6390bb6f87089fb64a0f908ca49963056a
Lori J <mintblue87@gmail.com>
2017-05-11 16:13:33 -0700

    specify pythonw environment for ani_depthplot

--- programs/ani_depthplot.py
+++ programs/ani_depthplot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env pythonw
 from __future__ import print_function
 from builtins import str
 import wx
@@ -16,7 +16,7 @@ import dialogs.pmag_menu_dialogs as pmag_menu_dialogs
 
 def main():
     """
-    NAME 
+    NAME
         ani_depthplot.py
 
     DESCRIPTION
@@ -29,10 +29,10 @@ def main():
         -h prints help message and quits
         -f FILE: specify input rmag_anisotropy format file from magic
         -fb FILE: specify input magic_measurements format file from magic
-        -fsa FILE: specify input er_samples format file from magic 
+        -fsa FILE: specify input er_samples format file from magic
         -fsum FILE : specify input LIMS database (IODP) core summary csv file
                 to print the core names, set lab to 1
-        -fa FILE: specify input er_ages format file from magic 
+        -fa FILE: specify input er_ages format file from magic
         -d min max [in m] depth range to plot
         -ds [mcd,mbsf], specify depth scale, default is mbsf
         -sav save plot without review
@@ -78,7 +78,7 @@ def main():
         else:
             print('Warning: Unrecognized option "{}" provided for depth scale.\nOptions for depth scale are mbsf -- meters below sea floor -- or mcd -- meters composite depth.\nAlternatively, if you provide an age file the depth scale will be automatically set to plot by age instead.\nUsing default "mbsf"'.format(depth_scale))
             depth_scale = 'sample_composite_depth'
-            
+
     fig, figname = ipmag.aniso_depthplot(ani_file, meas_file, samp_file, age_file, sum_file, fmt, dmin, dmax, depth_scale, dir_path)
     if save_quietly:
         if dir_path == '.':
@@ -87,7 +87,7 @@ def main():
         plt.clf()
         print('Saved file: {} in folder: {}'.format(figname, dir_path))
         return False
-    
+
     app = wx.App(redirect=False)
     if not fig:
         pw.simple_warning('No plot was able to be created with the data you provided.\nMake sure you have given all the required information and try again')
