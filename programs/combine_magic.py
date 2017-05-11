616eec3d68404a399489cfafd4914650e123495e
Lori J <mintblue87@gmail.com>
2017-05-11 12:11:45 -0700

    fix combine_magic.py for command line use

--- programs/combine_magic.py
+++ programs/combine_magic.py
@@ -11,7 +11,7 @@ SYNTAX
 
 OPTIONS
     -h prints help message
-    -dm specify data model (default: 2.5)
+    -dm specify data model (default: 3.0)
     -i allows interactive  entry of input and output filenames
     -WD specifies a directory to input and output files
     -F specify output file name [must come BEFORE input file names]
@@ -26,7 +26,7 @@ import pmagpy.command_line_extractor as extractor
 def combine(filenames,outfile,dir_path='.',data_model=2.5):
     files=[]
     for f in filenames.split():
-        files.append(os.path.join(dir_path,f)) 
+        files.append(os.path.join(dir_path,f))
     outfile=(os.path.join(dir_path,outfile))
     ipmag.combine_magic(files, outfile, data_model=data_model)
 
@@ -35,20 +35,23 @@ def main():
         help(__name__)
         sys.exit()
     if "-i" in sys.argv: # interactive
-        dataset,datasets=[],[]
+        dir_path = "."
+        dataset, datasets, filenames = [], [], []
+        data_model = 3
         while True:
             infile=input('\n\n Enter magic files for combining, <return>  when done: ')
             if infile=='':
                 break
             if os.path.isfile(infile):
-                filenames.append(infile)
+                filenames.append(os.path.realpath(infile))
             else:
                 print("-W- You have not provided a valid filename.\nIf the file is not in your current working directory, you will need to provide the full path to the file")
-        outfile=input('\n\n Enter name for new combined file')
+        outfile = input('\n\n Enter name for new combined file: ')
+        filenames = str(", ".join(filenames))
         if not outfile:
             return False
     else: # non-interactive
-        dataframe = extractor.command_line_dataframe([["F", True, '']])
+        dataframe = extractor.command_line_dataframe([["F", True, ''], ["dm", False, 3]])
         args = extractor.extract_and_check_args(sys.argv, dataframe)
         data_model, dir_path, outfile, filenames = extractor.get_vars(["dm","WD", "F", "f"], args)
     combine(filenames,outfile,dir_path,data_model)
