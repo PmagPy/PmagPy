579c11bbbe1a418bb909dfbdb020ef7dfbfed015
Lori J <mintblue87@gmail.com>
2017-05-12 12:31:22 -0700

    autopep8 programs/eqarea.py

--- programs/eqarea.py
+++ programs/eqarea.py
@@ -6,9 +6,10 @@ import os
 import numpy
 import matplotlib
 if matplotlib.get_backend() != "TKAgg":
-  matplotlib.use("TKAgg")
+    matplotlib.use("TKAgg")
 
 import pmagpy.pmagplotlib as pmagplotlib
+
 
 def main():
     """
@@ -36,58 +37,62 @@ def main():
           colors:  [b]lue,[g]reen,[r]ed,[c]yan,[m]agenta,[y]ellow,blac[k],[w]hite
 
     """
-    title=""
-    files,fmt={},'svg'
-    sym={'lower':['o','r'],'upper':['o','w']}
-    plot=0
-    if '-h' in sys.argv: # check if help is needed
+    title = ""
+    files, fmt = {}, 'svg'
+    sym = {'lower': ['o', 'r'], 'upper': ['o', 'w']}
+    plot = 0
+    if '-h' in sys.argv:  # check if help is needed
         print(main.__doc__)
-        sys.exit() # graceful quit
-    if '-sav' in sys.argv: plot=1
-    if '-fmt'  in sys.argv:
-        ind=sys.argv.index('-fmt')
-        fmt=sys.argv[ind+1]
-    if '-s'  in sys.argv:
-        ind=sys.argv.index('-s')
-        sym['size']=int(sys.argv[ind+1])
+        sys.exit()  # graceful quit
+    if '-sav' in sys.argv:
+        plot = 1
+    if '-fmt' in sys.argv:
+        ind = sys.argv.index('-fmt')
+        fmt = sys.argv[ind + 1]
+    if '-s' in sys.argv:
+        ind = sys.argv.index('-s')
+        sym['size'] = int(sys.argv[ind + 1])
     else:
-        sym['size']=20
-    if '-Lsym'  in sys.argv:
-        ind=sys.argv.index('-Lsym')
-        sym['lower'][0]=sys.argv[ind+1]
-        sym['lower'][1]=sys.argv[ind+2]
-    if '-Usym'  in sys.argv:
-        ind=sys.argv.index('-Usym')
-        sym['upper'][0]=sys.argv[ind+1]
-        sym['upper'][1]=sys.argv[ind+2]
-    if '-f' in sys.argv: # ask for filename
-        ind=sys.argv.index('-f')
-        fname=sys.argv[ind+1]
+        sym['size'] = 20
+    if '-Lsym' in sys.argv:
+        ind = sys.argv.index('-Lsym')
+        sym['lower'][0] = sys.argv[ind + 1]
+        sym['lower'][1] = sys.argv[ind + 2]
+    if '-Usym' in sys.argv:
+        ind = sys.argv.index('-Usym')
+        sym['upper'][0] = sys.argv[ind + 1]
+        sym['upper'][1] = sys.argv[ind + 2]
+    if '-f' in sys.argv:  # ask for filename
+        ind = sys.argv.index('-f')
+        fname = sys.argv[ind + 1]
     else:
         print(main.__doc__)
         print(' \n   -f option required')
-        sys.exit() # graceful quit
-    DI=numpy.loadtxt(fname)
-    EQ={'eq':1}
-    pmagplotlib.plot_init(EQ['eq'],5,5)
-    pmagplotlib.plotEQsym(EQ['eq'],DI,'Equal Area Plot',sym) # make plot
-    if plot==0:pmagplotlib.drawFIGS(EQ) # make it visible
+        sys.exit()  # graceful quit
+    DI = numpy.loadtxt(fname)
+    EQ = {'eq': 1}
+    pmagplotlib.plot_init(EQ['eq'], 5, 5)
+    pmagplotlib.plotEQsym(EQ['eq'], DI, 'Equal Area Plot', sym)  # make plot
+    if plot == 0:
+        pmagplotlib.drawFIGS(EQ)  # make it visible
     for key in list(EQ.keys()):
-        files[key]=key+'.'+fmt
+        files[key] = key + '.' + fmt
     if pmagplotlib.isServer:
-        black     = '#000000'
-        purple    = '#800080'
-        titles={}
-        titles['eq']='Equal Area Plot'
-        EQ = pmagplotlib.addBorders(EQ,titles,black,purple)
-        pmagplotlib.saveP(EQ,files)
-    elif plot==1:
+        black = '#000000'
+        purple = '#800080'
+        titles = {}
+        titles['eq'] = 'Equal Area Plot'
+        EQ = pmagplotlib.addBorders(EQ, titles, black, purple)
+        pmagplotlib.saveP(EQ, files)
+    elif plot == 1:
         fname = os.path.split(fname)[1].split('.')[0]
-        files['eq']=fname+'_eq.'+fmt
-        pmagplotlib.saveP(EQ,files)
+        files['eq'] = fname + '_eq.' + fmt
+        pmagplotlib.saveP(EQ, files)
     else:
-        ans=input(" S[a]ve to save plot, [q]uit without saving:  ")
-        if ans=="a": pmagplotlib.saveP(EQ,files)
+        ans = input(" S[a]ve to save plot, [q]uit without saving:  ")
+        if ans == "a":
+            pmagplotlib.saveP(EQ, files)
+
 
 if __name__ == "__main__":
     main()
