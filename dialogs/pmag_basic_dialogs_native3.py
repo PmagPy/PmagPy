c4bc5de95f3f5b73607dc191998cfd1012ff3452
Lori J <mintblue87@gmail.com>
2017-05-18 12:21:13 -0700

    make sure there are no “None” values in orientation grid, fixes #200

--- dialogs/pmag_basic_dialogs_native3.py
+++ dialogs/pmag_basic_dialogs_native3.py
@@ -2728,7 +2728,9 @@ class OrientFrameGrid3(wx.Frame):
                         val = val.name
                     except AttributeError:
                         pass
-                    self.grid.SetCellValue(sample_index, i, val)
+                    if val and val != "None":
+                        print('val', type(val), val)
+                        self.grid.SetCellValue(sample_index, i, val)
 
         #--------------------------------
 
