5d5dd3d0b5693ce4046de48af3b83a4311e2cf64
Lori J <mintblue87@gmail.com>
2017-10-31 13:27:10 -0700

    update run_tests to be verbose, and add test_imports3

--- run_tests.sh
+++ run_tests.sh
@@ -15,36 +15,38 @@
 #$use_this_python="$(which python)"
 
 echo Do Test Simple Example
-pythonw -m unittest pmagpy_tests.test_simple_example
+pythonw -m unittest -v pmagpy_tests.test_simple_example
 echo Do Test Pmag GUI
-pythonw -m unittest pmagpy_tests.test_pmag_gui
+pythonw -m unittest -v pmagpy_tests.test_pmag_gui
 echo Do Test Pmag
-pythonw -m unittest pmagpy_tests.test_pmag
+pythonw -m unittest -v pmagpy_tests.test_pmag
 echo Do Test Imports2
-pythonw -m unittest pmagpy_tests.test_imports2
+pythonw -m unittest -v pmagpy_tests.test_imports2
+echo Do Test Imports3
+pythonw -m unittest -v pmagpy_tests.test_imports3
 echo Do Test Ipmag
-pythonw -m unittest pmagpy_tests.test_ipmag
+pythonw -m unittest -v pmagpy_tests.test_ipmag
 echo Do Test Thellier GUI
-pythonw -m unittest pmagpy_tests.test_thellier_gui
+pythonw -m unittest -v pmagpy_tests.test_thellier_gui
 echo Do Test Demag GUI
-#pythonw -m unittest pmagpy_tests.test_demag_gui
+#pythonw -m unittest -v pmagpy_tests.test_demag_gui
 echo Do Test Magic GUI
-pythonw -m unittest pmagpy_tests.test_magic_gui
+pythonw -m unittest -v pmagpy_tests.test_magic_gui
 echo Do Test Builder
-pythonw -m unittest pmagpy_tests.test_builder
+pythonw -m unittest -v pmagpy_tests.test_builder
 echo Do Test Validations
-pythonw -m unittest pmagpy_tests.test_validations
+pythonw -m unittest -v pmagpy_tests.test_validations
 echo Do Test Programs
-#pythonw -m unittest pmagpy_tests.test_programs
+#pythonw -m unittest -v pmagpy_tests.test_programs
 echo Do Test Magic GUI 2
-pythonw -m unittest pmagpy_tests.test_magic_gui2
+pythonw -m unittest -v pmagpy_tests.test_magic_gui2
 echo Do Test New Builder
-pythonw -m unittest pmagpy_tests.test_new_builder
+pythonw -m unittest -v pmagpy_tests.test_new_builder
 echo Do Test Er Magic Dialogs
-pythonw -m unittest pmagpy_tests.test_er_magic_dialogs
+pythonw -m unittest -v pmagpy_tests.test_er_magic_dialogs
 echo Do Test Find Pmag Dir
-pythonw -m unittest pmagpy_tests.test_find_pmag_dir
+pythonw -m unittest -v pmagpy_tests.test_find_pmag_dir
 echo Do Test Map Magic
-pythonw -m unittest pmagpy_tests.test_map_magic
+pythonw -m unittest -v pmagpy_tests.test_map_magic
 echo Do Test Dialog Components
-pythonw -m unittest pmagpy_tests.test_dialog_components
+pythonw -m unittest -v pmagpy_tests.test_dialog_components
