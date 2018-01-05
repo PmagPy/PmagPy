2dc1f228c1ce87d61da9386f80102d2b858ab2a9
Lori J <mintblue87@gmail.com>
2018-01-05 11:00:52 -0800

    update mk_redo to data model 3, fixes #279

--- programs/mk_redo.py
+++ programs/mk_redo.py
@@ -17,49 +17,43 @@ def main():
         mk_redo.py [-h] [command line options]
 
     INPUT
-        takes pmag_specimens.txt formatted input file
+        takes specimens.txt formatted input file
 
     OPTIONS
         -h: prints help message and quits
-        -f FILE: specify input file, default is 'pmag_specimens.txt'
+        -f FILE: specify input file, default is 'specimens.txt'
         -F REDO: specify output file suffix, default is redo so that
             output filenames are 'thellier_redo' for thellier data and 'zeq_redo' for direction only data
 
     OUTPUT
         makes a thellier_redo or a zeq_redo format file
     """
-    dir_path = '.'
-    zfile, tfile = 'zeq_redo', 'thellier_redo'
-    inspec = "pmag_specimens.txt"
-    zredo, tredo = "", ""
-    if '-WD' in sys.argv:
-        ind = sys.argv.index('-WD')
-        dir_path = sys.argv[ind + 1]
     if '-h' in sys.argv:
         print(main.__doc__)
         sys.exit()
-    if '-f' in sys.argv:
-        ind = sys.argv.index('-f')
-        inspec = sys.argv[ind + 1]
+    zfile, tfile = 'zeq_redo', 'thellier_redo'
+    zredo, tredo = "", ""
+    dir_path = pmag.get_named_arg_from_sys('-WD', '.')
+    inspec = pmag.get_named_arg_from_sys('-f', 'specimens.txt')
     if '-F' in sys.argv:
         ind = sys.argv.index('-F')
         redo = sys.argv[ind + 1]
         tfile = redo
         zfile = redo
-    inspec = dir_path + "/" + inspec
-    zfile = dir_path + "/" + zfile
-    tfile = dir_path + "/" + tfile
+    inspec = pmag.resolve_file_name(inspec, dir_path)
+    zfile = pmag.resolve_file_name(zfile, dir_path)
+    tfile = pmag.resolve_file_name(tfile, dir_path)
 #
 # read in data
 #
     specs = []
     prior_spec_data, file_type = pmag.magic_read(inspec)
-    if file_type != 'pmag_specimens':
+    if file_type != 'specimens':
         print(file_type, " this is not a valid pmag_specimens file")
         sys.exit()
     outstrings = []
     for spec in prior_spec_data:
-        tmp = spec["magic_method_codes"].split(":")
+        tmp = spec["method_codes"].split(":")
         meths = []
         for meth in tmp:
             methods = meth.strip().split('-')
@@ -67,12 +61,12 @@ def main():
                 if m not in meths:
                     meths.append(m)
         if 'DIR' in meths:  # DE-BFL, DE-BFP or DE-FM
-            specs.append(spec['er_specimen_name'])
-            if 'specimen_comp_name' in list(spec.keys()) and spec['specimen_comp_name'] != "" and spec['specimen_comp_name'] != " ":
-                comp_name = spec['specimen_comp_name']
+            specs.append(spec['specimen'])
+            if 'dir_comp' in list(spec.keys()) and spec['dir_comp'] != "" and spec['dir_comp'] != " ":
+                comp_name = spec['dir_comp']
             else:
                 comp_name = string.ascii_uppercase[specs.count(
-                    spec['er_specimen_name']) - 1]
+                    spec['specimen']) - 1]
             calculation_type = "DE-BFL"  # assume default calculation type is best-fit line
             if "BFP" in meths:
                 calculation_type = 'DE-BFP'
@@ -81,15 +75,15 @@ def main():
             if zredo == "":
                 zredo = open(zfile, "w")
             outstring = '%s %s %s %s %s \n' % (
-                spec["er_specimen_name"], calculation_type, spec["measurement_step_min"], spec["measurement_step_max"], comp_name)
+                spec["specimen"], calculation_type, spec["meas_step_min"], spec["meas_step_max"], comp_name)
             if outstring not in outstrings:
                 zredo.write(outstring)
             outstrings.append(outstring)  # only writes unique interpretions
         elif "PI" in meths and "TRM" in meths:   # thellier record
             if tredo == "":
                 tredo = open(tfile, "w")
-            outstring = '%s %i %i \n' % (spec["er_specimen_name"], float(
-                spec["measurement_step_min"]), float(spec["measurement_step_max"]))
+            outstring = '%s %i %i \n' % (spec["specimen"], float(
+                spec["meas_step_min"]), float(spec["meas_step_max"]))
             if outstring not in outstrings:
                 tredo.write(outstring)
             outstrings.append(outstring)  # only writes unique interpretions
