65cdc99b3a05476f997751bd8c6cb10fe8901d75
Lori J <mintblue87@gmail.com>
2018-06-21 10:24:10 -0700

    fixes for irmaq_magic, #361

--- programs/irmaq_magic.py
+++ programs/irmaq_magic.py
@@ -99,6 +99,9 @@ def main():
         print('    Trying to acquire this information from {}'.format(dir_path))
         con = nb.Contribution(dir_path)
         meas_df = con.propagate_location_to_measurements()
+        if meas_df is None:
+            print('-W- No data found in {}'.format(dir_path))
+            return
         if plot_key not in meas_df.columns:
             print('-W- Could not find required data.')
             print('    Try a different plot key.')
@@ -178,11 +181,14 @@ def main():
             ans = input(" S[a]ve to save plot, [q]uit,  Return to continue:  ")
             if ans == 'q':
                 sys.exit()
-            if ans == "a":
+            elif ans == "a":
                 pmagplotlib.saveP(FIG, files)
+            elif plt != plotlist[-1]: # if it isn't the last plot, init the next one
+                pmagplotlib.plot_init(FIG['exp'], 6, 6)
         else:
             pmagplotlib.saveP(FIG, files)
         pmagplotlib.clearFIG(FIG['exp'])
+
 
 
 if __name__ == "__main__":
