b7f7932671a94708c2e51d1b3147d854dd2a2bf7
Lori J <mintblue87@gmail.com>
2018-07-11 18:06:11 -0700

    fix up mini_magic, in preparation for translating to data model 3, #382

--- programs/mini_magic.py
+++ programs/mini_magic.py
@@ -41,93 +41,52 @@ def main():
                  or e-mail ltauxe@ucsd.edu for help.
 
             [8] synthetic - has no site name
-            [9] ODP naming convention 
+            [9] ODP naming convention
             [10] LL-SI-SA-SP_STEP where LL is location, SI is site, SA is sample and SP is specimen and STEP is demagnetization step
 
     INPUT
-        Must put separate experiments (all AF, thermal,  etc.) in 
-           seperate files 
+        Must put separate experiments (all AF, thermal,  etc.) in
+           seperate files
 
-        Format of Yale MINI files:   
+        Format of Yale MINI files:
         LL-SI-SP_STEP, Declination, Inclination, Intensity (mA/m), X,Y,Z
 
 
     """
-# initialize some stuff
-    noave = 0
-    methcode, inst = "LP-NO", ""
-    samp_con, Z = '10', 1
-    demag = "N"
-    er_location_name = ""
-    citation = 'This study'
     args = sys.argv
-    coil = ""
-    volume = 10e-6  # assume a volume of 10cc
-#
-# get command line arguments
-#
-    user = ""
-    dir_path = './'
-    if '-WD' in args:
-        ind = args.index('-WD')
-        dir_path = args[ind+1]
     if "-h" in args:
         print(main.__doc__)
         sys.exit()
-    if "-usr" in args:
-        ind = args.index("-usr")
-        user = args[ind+1]
-    if '-F' in args:
-        ind = args.index("-F")
-        meas_file = dir_path+args[ind+1]
-    else:
-        meas_file = dir_path+'magic_measurements.txt'
-    if '-f' in args:
-        ind = args.index("-f")
-        magfile = dir_path+args[ind+1]
-        try:
-            input = open(magfile, 'r')
-        except:
-            print("bad mag file name")
-            sys.exit()
-    else:
-        print("mag_file field is required option")
-        print(main.__doc__)
-        sys.exit()
-    specnum = 0
-    if "-spc" in args:
-        ind = args.index("-spc")
-        specnum = int(args[ind+1])
-        if specnum != 0:
-            specnum = -specnum
-    if "-loc" in args:
-        ind = args.index("-loc")
-        er_location_name = args[ind+1]
-    if "-ins" in args:
-        ind = args.index("-ins")
-        inst = args[ind+1]
-    if "-vol" in args:
-        ind = args.index("-vol")
-        volume = 1e-6*float(args[ind+1])
+# initialize some stuff
+    methcode = "LP-NO"
+    demag = "N"
+    citation = 'This study'
+
+#
+# get command line arguments
+#
+    user = pmag.get_named_arg_from_sys("-usr", "")
+    dir_path = pmag.get_named_arg_from_sys("-WD", ".")
+    inst = pmag.get_named_arg_from_sys("-inst", "")
+    magfile = pmag.get_named_arg_from_sys("-f", reqd=True)
+    magfile = pmag.resolve_file_name(magfile, dir_path)
     if "-A" in args:
         noave = 1
-    if "-ncn" in args:
-        ind = args.index("-ncn")
-        samp_con = sys.argv[ind+1]
-        if "4" in samp_con:
-            if "-" not in samp_con:
-                print("option [4] must be in form 4-Z where Z is an integer")
-                sys.exit()
-            else:
-                Z = samp_con.split("-")[1]
-                samp_con = "4"
-        if "7" in samp_con:
-            if "-" not in samp_con:
-                print("option [7] must be in form 7-Z where Z is an integer")
-                sys.exit()
-            else:
-                Z = samp_con.split("-")[1]
-                samp_con = "4"
+    else:
+        noave = 0
+
+    try:
+        finput = open(magfile, 'r')
+        lines = finput.readlines()
+    except OSError:
+        print("bad mag file name")
+        sys.exit()
+    meas_file = pmag.get_named_arg_from_sys("-F", "magic_measurements.txt")
+    meas_file = pmag.resolve_file_name(meas_file, dir_path)
+    specnum = pmag.get_named_arg_from_sys("-spc", 0)
+    specnum = -specnum
+    volume = pmag.get_named_arg_from_sys("-vol", 10) # assume a volume of 10 cc if not provided
+    volume = 1e-6 * volume
     if '-LP' in args:
         ind = args.index("-LP")
         codelist = args[ind+1]
@@ -137,55 +96,81 @@ def main():
             methcode = "LT-AF-Z"
         if "T" in codes:
             demag = "T"
+    #
     MagRecs = []
     version_num = pmag.get_version()
-    for line in input.readlines():
+
+    #
+    spec_col = "er_specimen_name"
+    loc_col = "er_location_name"
+    site_col = "er_site_col"
+    samp_col = "er_sample_name"
+    software_col = "magic_software_packages"
+    treat_temp_col = "treatment_temp"
+    meas_temp_col = "measurement_temp"
+    treat_ac_col = "treatment_ac_field"
+    treat_dc_col = "treatment_dc_field"
+    treat_dc_phi_col = "treatment_dc_field_phi"
+    treat_dc_theta_col = "treatment_dc_field_theta"
+    treat_temp_col = "treatment_temp"
+    moment_col = "measurement_magn_moment"
+    dec_col = "measurement_dec"
+    inc_col = "measurement_inc"
+    instrument_col = "magic_instrument_codes"
+    analyst_col = "er_analyst_mail_names"
+    citations_col = "er_citation_names"
+    methods_col = "magic_method_codes"
+    quality_col = "measurement_flag"
+    meas_standard_col = "measurement_standard"
+    number_col = "measurement_number"
+    # go through the measurements
+    for line in lines:
         rec = line.split(',')
         if len(rec) > 1:
             MagRec = {}
             IDs = rec[0].split('_')
             treat = IDs[1]
-            MagRec["er_specimen_name"] = IDs[0]
-            print(MagRec['er_specimen_name'])
+            MagRec[spec_col] = IDs[0]
+            print(MagRec[spec_col])
             sids = IDs[0].split('-')
-            MagRec['er_location_name'] = sids[0]
-            MagRec['er_site_name'] = sids[0]+'-'+sids[1]
+            MagRec[loc_col] = sids[0]
+            MagRec[site_col] = sids[0]+'-'+sids[1]
             if len(sids) == 2:
-                MagRec["er_sample_name"] = IDs[0]
+                MagRec[samp_col] = IDs[0]
             else:
-                MagRec["er_sample_name"] = sids[0]+'-'+sids[1]+'-'+sids[2]
+                MagRec[samp_col] = sids[0]+'-'+sids[1]+'-'+sids[2]
             print(MagRec)
-            MagRec['magic_software_packages'] = version_num
-            MagRec["treatment_temp"] = '%8.3e' % (273)  # room temp in kelvin
-            MagRec["measurement_temp"] = '%8.3e' % (273)  # room temp in kelvin
-            MagRec["treatment_ac_field"] = '0'
-            MagRec["treatment_dc_field"] = '0'
-            MagRec["treatment_dc_field_phi"] = '0'
-            MagRec["treatment_dc_field_theta"] = '0'
+            MagRec[software_col] = version_num
+            MagRec[treat_temp_col] = '%8.3e' % (273)  # room temp in kelvin
+            MagRec[meas_temp_col] = '%8.3e' % (273)  # room temp in kelvin
+            MagRec[treat_ac_col] = '0'
+            MagRec[treat_dc_col] = '0'
+            MagRec[treat_dc_phi_col] = '0'
+            MagRec[treat_dc_theta_col] = '0'
             meas_type = "LT-NO"
             if demag == "AF":
-                MagRec["treatment_ac_field"] = '%8.3e' % (
+                MagRec[treat_ac_col] = '%8.3e' % (
                     float(treat)*1e-3)  # peak field in tesla
             if demag == "T":
                 meas_type = "LT-T-Z"
-                MagRec["treatment_dc_field"] = '%8.3e' % (0)
-                MagRec["treatment_temp"] = '%8.3e' % (
+                MagRec[treat_dc_col] = '%8.3e' % (0)
+                MagRec[treat_temp_col] = '%8.3e' % (
                     float(treat)+273.)  # temp in kelvin
             if demag == "N":
                 meas_type = "LT-NO"
-                MagRec["treatment_ac_field"] = '0'
-                MagRec["treatment_dc_field"] = '0'
-            MagRec["measurement_magn_moment"] = '%10.3e' % (
+                MagRec[treat_ac_col] = '0'
+                MagRec[treat_dc_col] = '0'
+            MagRec[moment_col] = '%10.3e' % (
                 volume*float(rec[3])*1e-3)  # moment in Am2 (from mA/m)
-            MagRec["measurement_dec"] = rec[1]
-            MagRec["measurement_inc"] = rec[2]
-            MagRec["magic_instrument_codes"] = inst
-            MagRec["er_analyst_mail_names"] = user
-            MagRec["er_citation_names"] = citation
-            MagRec["magic_method_codes"] = methcode.strip(':')
-            MagRec["measurement_flag"] = 'g'
-            MagRec["measurement_standard"] = 'u'
-            MagRec["measurement_number"] = '1'
+            MagRec[dec_col] = rec[1]
+            MagRec[inc_col] = rec[2]
+            MagRec[instrument_col] = inst
+            MagRec[analyst_col] = user
+            MagRec[citations_col] = citation
+            MagRec[methods_col] = methcode.strip(':')
+            MagRec[quality_col] = 'g'
+            MagRec[meas_standard_col] = 'u'
+            MagRec[number_col] = '1'
             MagRecs.append(MagRec)
     MagOuts = pmag.measurements_methods(MagRecs, noave)
     pmag.magic_write(meas_file, MagOuts, 'magic_measurements')
