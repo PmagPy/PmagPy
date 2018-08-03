884a2e608d914a02086aa11930fcb55ac7a35f3a
Lori J <mintblue87@gmail.com>
2018-08-03 13:14:20 -0700

    gently clean up grab_magic_key and magic_select, #405

--- programs/magic_select.py
+++ programs/magic_select.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 import pmagpy.pmag as pmag
 
+
 def main():
     """
     NAME
@@ -33,50 +34,58 @@ def main():
          for paleolat:
              use KEY: model_lat (uses lat, if age<5 Ma, else, model_lat, or attempts calculation from average_inc if no model_lat.) returns estimate in model_lat key
 
+      EXAMPLE:
+         # here I want to output all records where the site column exactly matches "MC01"
+         magic_select.py -f samples.txt -key site MC01 T -F select_samples.txt
+
     """
-    dir_path="."
-    flag=''
+    dir_path = "."
+    flag = ''
     if '-WD' in sys.argv:
-        ind=sys.argv.index('-WD')
-        dir_path=sys.argv[ind+1]
+        ind = sys.argv.index('-WD')
+        dir_path = sys.argv[ind+1]
     if '-h' in sys.argv:
         print(main.__doc__)
         sys.exit()
     if '-f' in sys.argv:
-        ind=sys.argv.index('-f')
-        magic_file=dir_path+'/'+sys.argv[ind+1]
+        ind = sys.argv.index('-f')
+        magic_file = dir_path+'/'+sys.argv[ind+1]
     else:
         print(main.__doc__)
+        print('-W- "-f" is a required option')
         sys.exit()
     if '-F' in sys.argv:
-        ind=sys.argv.index('-F')
-        outfile=dir_path+'/'+sys.argv[ind+1]
+        ind = sys.argv.index('-F')
+        outfile = dir_path+'/'+sys.argv[ind+1]
     else:
         print(main.__doc__)
+        print('-W- "-F" is a required option')
         sys.exit()
     if '-key' in sys.argv:
-        ind=sys.argv.index('-key')
-        grab_key=sys.argv[ind+1]
-        v=sys.argv[ind+2]
-        flag=sys.argv[ind+3]
+        ind = sys.argv.index('-key')
+        grab_key = sys.argv[ind+1]
+        v = sys.argv[ind+2]
+        flag = sys.argv[ind+3]
     else:
         print(main.__doc__)
         print('-key is required')
         sys.exit()
     #
     # get data read in
-    Data,file_type=pmag.magic_read(magic_file)
-    if grab_key =='age':
-        grab_key='average_age'
-        Data=pmag.convert_ages(Data)
-    if grab_key =='model_lat':
-        Data=pmag.convert_lat(Data)
-        Data=pmag.convert_ages(Data)
-    Selection=pmag.get_dictitem(Data,grab_key,v,flag,float_to_int=True)
-    if len(Selection)>0:
-        pmag.magic_write(outfile,Selection,file_type)
+    Data, file_type = pmag.magic_read(magic_file)
+    if grab_key == 'age':
+        grab_key = 'average_age'
+        Data = pmag.convert_ages(Data)
+    if grab_key == 'model_lat':
+        Data = pmag.convert_lat(Data)
+        Data = pmag.convert_ages(Data)
+    print(Data[0])
+    Selection = pmag.get_dictitem(Data, grab_key, v, flag, float_to_int=True)
+    if len(Selection) > 0:
+        pmag.magic_write(outfile, Selection, file_type)
     else:
         print('no data matched your criteria')
+
 
 if __name__ == "__main__":
     main()
