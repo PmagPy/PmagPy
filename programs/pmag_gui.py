b9703ec414c3a8252324415bb56c0fead72d2024
Lisa Tauxe <ltauxe@ucsd.edu>
2018-11-27 14:37:14 -0800

    modified:   pmag_gui.py

--- programs/pmag_gui.py
+++ programs/pmag_gui.py
@@ -2,14 +2,13 @@
 
 # pylint: disable=W0612,C0111,C0103,W0201,E402
 
-import sys
-print (sys.version)
 print("-I- Importing Pmag GUI dependencies")
 #from pmag_env import set_env
 #set_env.set_backend(wx=True)
 import matplotlib
 if not matplotlib.get_backend() == 'WXAgg':
     matplotlib.use('WXAgg')
+import sys
 import wx
 import wx.lib.buttons as buttons
 import wx.lib.newevent as newevent
