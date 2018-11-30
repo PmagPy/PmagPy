2861c715fb269310117f57dfd151f6b6655a28c0
Lori J <mintblue87@gmail.com>
2018-11-30 13:10:59 -0800

    split dmag_magic into plot/main, add tests #424

--- pmagpy_tests/test_plots.py
+++ pmagpy_tests/test_plots.py
@@ -3,6 +3,7 @@ import unittest
 import glob
 from programs import polemap_magic
 from programs import eqarea_magic
+from programs import dmag_magic
 from pmagpy import pmag
 WD = pmag.get_test_WD()
 
@@ -47,4 +48,42 @@ class TestEqareaMagic(unittest.TestCase):
 
     def test_failure(self):
         res, outfiles = eqarea_magic.plot_eq(dir_path="data_files/", save_plots=True, fmt="png", plot_by="sit")
+        self.assertFalse(res)
+
+
+
+class TestDmagMagic(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        glob_strings = ["*.png", "*.svg"]
+        for string in glob_strings:
+            for fname in glob.glob(string):
+                os.remove(fname)
+
+    def test_success(self):
+        res, outfiles = dmag_magic.plot(dir_path=".", input_dir_path="data_files/3_0/McMurdo", LT="T", plot_by='sit')
+        self.assertTrue(res)
+        for f in outfiles:
+            self.assertTrue(os.path.exists(f))
+        images = glob.glob("*.svg")
+        self.assertEqual(len(images), 126)
+
+    def test_alt_success(self):
+        res, outfiles = dmag_magic.plot(dir_path=".", input_dir_path="data_files/3_0/McMurdo", plot_by='spc', fmt="png")
+        self.assertTrue(res)
+        for f in outfiles:
+            self.assertTrue(os.path.exists(f))
+        images = glob.glob("*.png")
+        self.assertEqual(len(images), 530)
+
+    def test_failure(self):
+        res, outfiles = dmag_magic.plot(dir_path=".", input_dir_path="data_files/3_0/McMurdo", plot_by='spc',
+                                        fmt="png", LT="FAKE")
+        self.assertFalse(res)
+
+    def test_alt_failure(self):
+        res, outfiles = dmag_magic.plot(dir_path=".", input_dir_path="data_files/3_0/", plot_by='spc')
         self.assertFalse(res)
--- programs/dmag_magic.py
+++ programs/dmag_magic.py
@@ -13,6 +13,128 @@ import pmagpy.pmagplotlib as pmagplotlib
 import pmagpy.contribution_builder as cb
 
 
+def plot(in_file="measurements.txt", dir_path=".", input_dir_path="",
+         spec_file="specimens.txt", samp_file="samples.txt",
+         site_file="sites.txt", loc_file="locations.txt",
+         plot_by="loc", LT="AF", norm=True, XLP="",
+         save_plots=True, fmt="svg"):
+
+    # format plot_key
+    name_dict = {'loc': 'location', 'sit': 'site',
+                 'sam': 'sample', 'spc': 'specimen'}
+    if plot_by not in name_dict.values():
+        try:
+            plot_key = name_dict[plot_by]
+        except KeyError:
+            print('Unrecognized plot_by {}, falling back to plot by location'.format(plot_by))
+            plot_key = "loc"
+    else:
+        plot_key = plot_by
+
+
+    # figure out what kind of experiment
+    LT = "LT-" + LT + "-Z"
+    print('LT', LT)
+    if LT == "LT-T-Z":
+        units, dmag_key = 'K', 'treat_temp'
+    elif LT == "LT-AF-Z":
+        units, dmag_key = 'T', 'treat_ac_field'
+    elif LT == 'LT-M-Z':
+        units, dmag_key = 'J', 'treat_mw_energy'
+    else:
+        units = 'U'
+
+
+    # init
+    FIG = {}  # plot dictionary
+    FIG['demag'] = 1  # demag is figure 1
+    # create contribution and add required headers
+    fnames = {"specimens": spec_file, "samples": samp_file,
+              'sites': site_file, 'locations': loc_file}
+    if not os.path.exists(pmag.resolve_file_name(in_file, input_dir_path)):
+        print('-E- Could not find {}'.format(in_file))
+        return False, []
+    contribution = cb.Contribution(input_dir_path, single_file=in_file,
+                                   custom_filenames=fnames)
+    file_type = list(contribution.tables.keys())[0]
+    print(len(contribution.tables['measurements'].df), ' records read from ', in_file)
+    # add plot_key into measurements table
+    if plot_key not in contribution.tables['measurements'].df.columns:
+        #contribution.propagate_name_down(plot_key, 'measurements')
+        contribution.propagate_location_to_measurements()
+    data_container = contribution.tables[file_type]
+    # pare down to only records with useful data
+    # grab records that have the requested code
+    data_slice = data_container.get_records_for_code(LT)
+    # and don't have the offending code
+    data = data_container.get_records_for_code(XLP, incl=False, use_slice=True,
+                                               sli=data_slice, strict_match=False)
+
+    # make sure quality is in the dataframe
+    if 'quality' not in data.columns:
+        data['quality'] = 'g'
+    # get intensity key and make sure intensity data is not blank
+    intlist = ['magn_moment', 'magn_volume', 'magn_mass']
+    IntMeths = [col_name for col_name in data.columns if col_name in intlist]
+    # get rid of any entirely blank intensity columns
+    for col_name in IntMeths:
+        if not data[col_name].any():
+            data.drop(col_name, axis=1, inplace=True)
+    IntMeths = [col_name for col_name in data.columns if col_name in intlist]
+    if len(IntMeths) == 0:
+        print('-E- No intensity headers found')
+        return False, []
+
+    int_key = IntMeths[0] # plot first intensity method found - normalized to initial value anyway - doesn't matter which used
+    data = data[data[int_key].notnull()]
+    # make list of individual plots
+    # by default, will be by location_name
+    plotlist = data[plot_key].unique()
+    plotlist.sort()
+    pmagplotlib.plot_init(FIG['demag'], 5, 5)
+    last_plot = False
+    # iterate through and plot the data
+    for plt in plotlist:
+        if plt == plotlist[-1]:
+            last_plot = True
+        plot_data = data[data[plot_key] == plt].copy()
+        if not save_plots:
+            print(plt, 'plotting by: ', plot_key)
+        if len(plot_data) > 2:
+            title = plt
+            spcs = []
+            spcs = plot_data['specimen'].unique()
+            for spc in spcs:
+                INTblock = []
+                spec_data = plot_data[plot_data['specimen'] == spc]
+                for ind, rec in spec_data.iterrows():
+                    INTblock.append([float(rec[dmag_key]), 0, 0, float(rec[int_key]), 1, rec['quality']])
+                if len(INTblock) > 2:
+                    pmagplotlib.plot_mag(FIG['demag'], INTblock,
+                                       title, 0, units, norm)
+
+            if save_plots:
+                files = {}
+                for key in list(FIG.keys()):
+                    files[key] = title + '_' + LT + '.' + fmt
+                pmagplotlib.save_plots(FIG, files)
+            else:
+                pmagplotlib.draw_figs(FIG)
+                prompt = " S[a]ve to save plot, [q]uit,  Return to continue:  "
+                ans = input(prompt)
+                if ans == 'q':
+                    return True, []
+                if ans == "a":
+                    files = {}
+                    for key in list(FIG.keys()):
+                        files[key] = title + '_' + LT + '.' + fmt
+                    pmagplotlib.save_plots(FIG, files)
+            pmagplotlib.clearFIG(FIG['demag'])
+    if last_plot:
+        return True, []
+
+
+
 def main():
     """
     NAME
@@ -45,109 +167,34 @@ def main():
         print(main.__doc__)
         sys.exit()
     # initialize variables from command line + defaults
-    FIG = {}  # plot dictionary
-    FIG['demag'] = 1  # demag is figure 1
+
+    dir_path = pmag.get_named_arg("-WD", default_val=".")
+    input_dir_path = pmag.get_named_arg('-ID', '')
+    if not input_dir_path:
+        input_dir_path = dir_path
     in_file = pmag.get_named_arg("-f", default_val="measurements.txt")
+    in_file = pmag.resolve_file_name(in_file, input_dir_path)
+    if "-ID" not in sys.argv:
+        input_dir_path = os.path.split(in_file)[0]
+
     plot_by = pmag.get_named_arg("-obj", default_val="loc")
-    name_dict = {'loc': 'location', 'sit': 'site',
-                 'sam': 'sample', 'spc': 'specimen'}
-    plot_key = name_dict[plot_by]
-    LT = "LT-" + pmag.get_named_arg("-LT", "AF") + "-Z"
-    if LT == "LT-T-Z":
-        units, dmag_key = 'K', 'treat_temp'
-    elif LT == "LT-AF-Z":
-        units, dmag_key = 'T', 'treat_ac_field'
-    elif LT == 'LT-M-Z':
-        units, dmag_key = 'J', 'treat_mw_energy'
-    else:
-        units = 'U'
+    LT = pmag.get_named_arg("-LT", "AF")
     no_norm = pmag.get_flag_arg_from_sys("-N")
     norm = 0 if no_norm else 1
-    no_plot = pmag.get_flag_arg_from_sys("-sav")
-    plot = 0 if no_plot else 1
+
+    save_plots = pmag.get_flag_arg_from_sys("-sav")
+    #no_plot = pmag.get_flag_arg_from_sys("-sav")
+    #plot = 0 if no_plot else 1
     fmt = pmag.get_named_arg("-fmt", "svg")
     XLP = pmag.get_named_arg("-XLP", "")
     dir_path = pmag.get_named_arg("-WD", os.getcwd())
     spec_file = pmag.get_named_arg("-fsp", default_val="specimens.txt")
     samp_file = pmag.get_named_arg("-fsa", default_val="samples.txt")
     site_file = pmag.get_named_arg("-fsi", default_val="sites.txt")
-
-    # create contribution and add required headers
-    fnames = {"specimens": spec_file, "samples": samp_file, 'sites': site_file}
-    contribution = cb.Contribution(dir_path, single_file=in_file,
-                                   custom_filenames=fnames)
-    file_type = list(contribution.tables.keys())[0]
-    print(len(contribution.tables['measurements'].df), ' records read from ', in_file)
-    # add plot_key into measurements table
-    if plot_key not in contribution.tables['measurements'].df.columns:
-        #contribution.propagate_name_down(plot_key, 'measurements')
-        contribution.propagate_location_to_measurements()
-    data_container = contribution.tables[file_type]
-    # pare down to only records with useful data
-    # grab records that have the requested code
-    data_slice = data_container.get_records_for_code(LT)
-    # and don't have the offending code
-    data = data_container.get_records_for_code(XLP, incl=False, use_slice=True,
-                                               sli=data_slice, strict_match=False)
-
-    # make sure quality is in the dataframe
-    if 'quality' not in data.columns:
-        data['quality'] = 'g'
-    # get intensity key and make sure intensity data is not blank
-    intlist = ['magn_moment', 'magn_volume', 'magn_mass']
-    IntMeths = [col_name for col_name in data.columns if col_name in intlist]
-    # get rid of any entirely blank intensity columns
-    for col_name in IntMeths:
-        if not data[col_name].any():
-            data.drop(col_name, axis=1, inplace=True)
-    IntMeths = [col_name for col_name in data.columns if col_name in intlist]
-    if len(IntMeths) == 0:
-        print('No intensity headers found')
-        sys.exit()
-
-    int_key = IntMeths[0] # plot first intensity method found - normalized to initial value anyway - doesn't matter which used
-    data = data[data[int_key].notnull()]
-    # make list of individual plots
-    # by default, will be by location_name
-    plotlist = data[plot_key].unique()
-    plotlist.sort()
-    pmagplotlib.plot_init(FIG['demag'], 5, 5)
-    # iterate through and plot the data
-    for plt in plotlist:
-        plot_data = data[data[plot_key] == plt].copy()
-        if plot:
-            print(plt, 'plotting by: ', plot_key)
-        if len(plot_data) > 2:
-            title = plt
-            spcs = []
-            spcs = plot_data['specimen'].unique()
-            for spc in spcs:
-                INTblock = []
-                spec_data = plot_data[plot_data['specimen'] == spc]
-                for ind, rec in spec_data.iterrows():
-                    INTblock.append([float(rec[dmag_key]), 0, 0, float(rec[int_key]), 1, rec['quality']])
-                if len(INTblock) > 2:
-                    pmagplotlib.plot_mag(FIG['demag'], INTblock,
-                                       title, 0, units, norm)
-
-            if not plot:
-                files = {}
-                for key in list(FIG.keys()):
-                    files[key] = title + '_' + LT + '.' + fmt
-                pmagplotlib.save_plots(FIG, files)
-                #sys.exit()
-            else:
-                pmagplotlib.draw_figs(FIG)
-                prompt = " S[a]ve to save plot, [q]uit,  Return to continue:  "
-                ans = input(prompt)
-                if ans == 'q':
-                    sys.exit()
-                if ans == "a":
-                    files = {}
-                    for key in list(FIG.keys()):
-                        files[key] = title + '_' + LT + '.' + fmt
-                    pmagplotlib.save_plots(FIG, files)
-            pmagplotlib.clearFIG(FIG['demag'])
+    loc_file = pmag.get_named_arg("-flo", default_val="locations.txt")
+    plot(in_file, dir_path, input_dir_path, spec_file, samp_file,
+         site_file, loc_file, plot_by, LT, norm, XLP,
+         save_plots, fmt)
 
 if __name__ == "__main__":
     main()
