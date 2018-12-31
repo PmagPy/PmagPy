ba3a8bb24837ff888a5d9178ccb5b99a3eb245f3
Lori J <mintblue87@gmail.com>
2018-12-31 14:01:04 -0800

    increment version #

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -4,5 +4,5 @@ Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
 
-"pmagpy-4.2.15"
-version = 'pmagpy-4.2.15'
+"pmagpy-4.2.17"
+version = 'pmagpy-4.2.17'
