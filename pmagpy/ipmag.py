96f683062b0c792595e08bd3deeee9701111bf02
Lori J <mintblue87@gmail.com>
2019-02-18 11:22:21 -0800

    re-set ipmag.zeq_magic save_plots to True

--- pmagpy/ipmag.py
+++ pmagpy/ipmag.py
@@ -10057,7 +10057,7 @@ def atrm_magic(meas_file, dir_path=".", input_dir_path="",
 
 
 def zeq_magic(meas_file='measurements.txt', spec_file='',crd='s',input_dir_path='.', angle=0,
-              n_plots=5, save_plots=False, fmt="svg", interactive=False):
+              n_plots=5, save_plots=True, fmt="svg", interactive=False):
     """
     zeq_magic makes zijderveld and equal area plots for magic formatted measurements files.
 
@@ -10070,7 +10070,7 @@ def zeq_magic(meas_file='measurements.txt', spec_file='',crd='s',input_dir_path=
     samp_file : str
         input sample orientations file
     crd : str
-        coordinate system [s,g,t] for specimen, geographic, tilt corrected 
+        coordinate system [s,g,t] for specimen, geographic, tilt corrected
         g,t options require a sample file with specimen and bedding orientation
     input_dir_path : str
         input directory of meas_file, default "."
