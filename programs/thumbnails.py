d42b36b5bbe5d0d773a661f8b3fd71e1b865dd97
Lori J <mintblue87@gmail.com>
2019-02-20 12:24:01 -0800

    might as well support multiple format options, #518

--- programs/thumbnails.py
+++ programs/thumbnails.py
@@ -8,23 +8,23 @@ import glob
 from PIL import Image
 from pmagpy import pmag
 
-def main(directory):
+def main(directory, fmt):
     # get all the jpg files from the current folder
     directory = os.path.realpath(directory)
-    for infile in glob.glob(os.path.join(directory, "*.png")):
-        if infile.endswith('thumb.png'):
+    for infile in glob.glob(os.path.join(directory, "*.{}".format(fmt))):
+        if infile.endswith('thumb.{}'.format(fmt)):
             continue
         im = Image.open(infile)
     # convert to thumbnail image
         im.thumbnail((128, 128), Image.ANTIALIAS)
-    # prefix thumbnail file with T_
-        im.save(infile[:-4] + ".thumb.png", "PNG")
+    # add .thumb to file extension
+        im.save(infile[:-4] + ".thumb.{}".format(fmt), fmt)
 
 
 if __name__ == "__main__":
     if "-h" in sys.argv:
         print('Make thumbnails for every png in current directory, or use: thumbnails.py -WD <directory>')
-        print('Output files will follow the naming convention: "original_filename.thumb.png"')
+        print('Specify input/output format with: -fmt <format>.   Tested formats include [jpg, png]')
+        print('Output files will follow the naming convention: "original_filename.thumb.fmt"')
     else:
-        directory = pmag.get_named_arg("-WD", ".")
-        main(directory)
+        main(pmag.get_named_arg("-WD", "."), pmag.get_named_arg("-fmt", "png"))
