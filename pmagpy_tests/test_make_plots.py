dd1e3a4479fceec07f2457a32621b6c231c725f1
Lori J <mintblue87@gmail.com>
2019-02-22 11:10:50 -0800

    make_magic_plots tests must take into account whether isServer is True or not, #518

--- pmagpy_tests/test_make_plots.py
+++ pmagpy_tests/test_make_plots.py
@@ -5,12 +5,12 @@ import os
 import random
 import glob
 from pmagpy import pmag
+from pmagpy import pmagplotlib
 from pmag_env import set_env
-
 
 WD = pmag.get_test_WD()
 
-@unittest.skipIf(not pmagplotlib.isServer, "these tests only work when isServer is True")
+#@unittest.skipIf(not pmagplotlib.isServer, "these tests only work when isServer is True")
 class TestMakeMagicPlots(unittest.TestCase):
 
     def setUp(self):
@@ -28,16 +28,16 @@ class TestMakeMagicPlots(unittest.TestCase):
 
     def test_make_plots(self):
         os.chdir(os.path.join(WD, 'data_files', '3_0', 'Osler'))
-        print('os.getcwd()',os.getcwd())
-        print('listdir', os.listdir())
         for filename in glob.glob("*error*"):
             os.remove(filename)
         os.system("make_magic_plots.py")
-        print('listdir', os.listdir())
         self.assertFalse(glob.glob("errors.txt"))
-        self.assertFalse(glob.glob("thumbnail_errors.txt"))
-        self.assertEqual(14, len(glob.glob("*.png")))
-        self.assertEqual(7, len(glob.glob("*thumb*.png")))
+        if pmagplotlib.isServer:
+            self.assertFalse(glob.glob("thumbnail_errors.txt"))
+            self.assertEqual(14, len(glob.glob("*.png")))
+            self.assertEqual(7, len(glob.glob("*thumb*.png")))
+        else:
+            self.assertEqual(5, len(glob.glob("*.png")))
 
     def test_make_plots_long(self):
         # only run this annoyingly slow test 10% of the time
@@ -45,12 +45,12 @@ class TestMakeMagicPlots(unittest.TestCase):
         if num != 3:
             return
         os.chdir(os.path.join(WD, 'data_files', '3_0', 'McMurdo'))
-        print('os.getcwd()',os.getcwd())
         for filename in glob.glob("*error*"):
             os.remove(filename)
         os.system("make_magic_plots.py")
         self.assertFalse(glob.glob("errors.txt"))
-        self.assertFalse(glob.glob("thumbnail_errors.txt"))
-        num_pngs = len(glob.glob("*png"))
-        num_thumbnails = len(glob.glob("*thumb.png"))
-        self.assertEqual(num_pngs / 2, num_thumbnails)
+        if pmagplotlib.isServer:
+            self.assertFalse(glob.glob("thumbnail_errors.txt"))
+            num_pngs = len(glob.glob("*png"))
+            num_thumbnails = len(glob.glob("*thumb.png"))
+            self.assertEqual(num_pngs / 2, num_thumbnails)
