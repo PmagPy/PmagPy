f7582de0f1dcf595c6b8ce56b9c1a2c48bcb7479
Lori J <mintblue87@gmail.com>
2019-04-16 15:29:26 -0700

    initial histplot refactor, #558

--- programs/histplot.py
+++ programs/histplot.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
+from pmagpy import pmag
+from pmagpy import pmagplotlib
+from matplotlib import pyplot as plt
 import sys
+import os
 import numpy as np
 import matplotlib
 if matplotlib.get_backend() != "TKAgg":
     matplotlib.use("TKAgg")
-from matplotlib import pyplot as plt
-from pmagpy import pmagplotlib
 
 
 def main():
@@ -35,49 +37,53 @@ def main():
        histplot.py [command line options] [<file]
 
     """
-    fname, fmt = "", 'svg'
-    plot = 0
+    save_plots = False
     if '-sav' in sys.argv:
-        plot = 1
+        save_plots = True
+        interactive = False
+
     if '-h' in sys.argv:
         print(main.__doc__)
         sys.exit()
-    if '-fmt' in sys.argv:
-        ind = sys.argv.index('-fmt')
-        fmt = sys.argv[ind+1]
-    if '-f' in sys.argv:
-        ind = sys.argv.index('-f')
-        fname = sys.argv[ind+1]
-    if '-F' in sys.argv:
-        ind = sys.argv.index('-F')
-        outfile = sys.argv[ind+1]
-        fmt = ""
-    else:
-        outfile = 'hist.'+fmt
+    fmt = pmag.get_named_arg('-fmt', 'svg')
+    fname = pmag.get_named_arg('-f', '')
+    outfile = pmag.get_named_arg("-F", "")
+    norm = 1
     if '-N' in sys.argv:
         norm = 0
-        ylab = 'Number'
-    else:
-        norm = 1
-        ylab = 'Frequency'
     if '-twin' in sys.argv:
-        norm=-1
-    binsize = 0
-    if '-b' in sys.argv:
-        ind = sys.argv.index('-b')
-        binsize = int(sys.argv[ind+1])
+        norm = - 1
+    binsize = pmag.get_named_arg('-b', 0)
     if '-xlab' in sys.argv:
         ind = sys.argv.index('-xlab')
         xlab = sys.argv[ind+1]
     else:
         xlab = 'x'
-    if fname != "":
-        D = np.loadtxt(fname)
-    else:
+    data = []
+    if not fname:
         print('-I- Trying to read from stdin... <ctrl>-c to quit')
-        D = np.loadtxt(sys.stdin, dtype=np.float)
+        data = np.loadtxt(sys.stdin, dtype=np.float)
+
+    histplot(fname, data, outfile, xlab, binsize, norm,
+             fmt, save_plots, interactive)
     # read in data
     #
+
+
+def histplot(infile="", data=(), outfile="",
+             xlab='x', binsize=0, norm=1,
+             fmt='svg', save_plots=True, interactive=False):
+    # set outfile name
+    if outfile:
+        fmt = ""
+    else:
+        outfile = 'hist.'+fmt
+    # read in data from infile or use data argument
+    if os.path.exists(infile):
+        D = np.loadtxt(infile)
+    else:
+        D = np.array(data)
+
     try:
         if not D:
             print('-W- No data found')
@@ -92,43 +98,49 @@ def main():
     if len(D) < 5:
         print("-W- Not enough points to plot histogram ({} point(s) provided, 5 required)".format(len(D)))
         return
+
     # if binsize not provided, calculate reasonable binsize
     if not binsize:
         binsize = int(np.around(1 + 3.22 * np.log(len(D))))
     Nbins = int(len(D) / binsize)
     ax = fig.add_subplot(111)
-    if norm==1:
-        print ('normalizing')
-        n, bins, patches = ax.hist(
-            D, bins=Nbins, facecolor='#D3D3D3', histtype='stepfilled', color='black', density=True)
-        ax.set_ylabel(ylab)
-    elif norm==0:
-        print ('not normalizing')
-        n, bins, patches = ax.hist(
-            D, bins=Nbins, facecolor='#D3D3D3', histtype='stepfilled', color='black', density=False)
-        ax.set_ylabel(ylab)
-    elif norm==-1:
-        print ('trying twin')
+    if norm == 1:
+        print('normalizing')
         n, bins, patches = ax.hist(
             D, bins=Nbins, facecolor='#D3D3D3', histtype='stepfilled', color='black', density=True)
         ax.set_ylabel('Frequency')
-        ax2=ax.twinx()
+    elif norm == 0:
+        print('not normalizing')
+        n, bins, patches = ax.hist(
+            D, bins=Nbins, facecolor='#D3D3D3', histtype='stepfilled', color='black', density=False)
+        ax.set_ylabel('Number')
+    elif norm == -1:
+        print('trying twin')
+        n, bins, patches = ax.hist(
+            D, bins=Nbins, facecolor='#D3D3D3', histtype='stepfilled', color='black', density=True)
+        ax.set_ylabel('Frequency')
+        ax2 = ax.twinx()
         n, bins, patches = ax2.hist(
             D, bins=Nbins, facecolor='#D3D3D3', histtype='stepfilled', color='black', density=False)
-        ax2.set_ylabel('Number',rotation=-90)
+        ax2.set_ylabel('Number', rotation=-90)
     plt.axis([D.min(), D.max(), 0, n.max()+.1*n.max()])
     ax.set_xlabel(xlab)
     name = 'N = ' + str(len(D))
     plt.title(name)
-    if plot == 0:
+    if interactive:
         pmagplotlib.draw_figs({1: 'hist'})
         p = input('s[a]ve to save plot, [q]uit to exit without saving  ')
         if p != 'a':
-            sys.exit()
+            return True, []
+        plt.savefig(outfile)
+        print('plot saved in ', outfile)
+        return True, [outfile]
     if pmagplotlib.isServer:
         pmagplotlib.add_borders({'hist': 1}, {'hist': 'Intensity Histogram'})
-    plt.savefig(outfile)
-    print('plot saved in ', outfile)
+    if save_plots:
+        plt.savefig(outfile)
+        print('plot saved in ', outfile)
+        return True, [outfile]
 
 
 if __name__ == "__main__":
