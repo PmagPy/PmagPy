403e8839fa718531edbfdae84ef9ecaf5fbbad38
Lori J <mintblue87@gmail.com>
2019-04-18 15:02:32 -0700

    fix all syntax in pmagpy_tests/__init__

--- pmagpy_tests/__init__.py
+++ pmagpy_tests/__init__.py
@@ -8,18 +8,18 @@ if "-pip" in sys.argv:
 
 from pmagpy_tests import test_builder
 from pmagpy_tests import test_er_magic_dialogs
-import pmagpy_tests.test_ipmag as test_ipmag
-import pmagpy_tests.test_magic_gui as test_magic_gui
-import pmagpy_tests.test_pmag as test_pmag
-import pmagpy_tests.test_pmag_gui as test_pmag_gui
-import pmagpy_tests.test_thellier_gui as test_thellier_gui
-import pmagpy_tests.test_validations as test_validations
-import pmagpy_tests.test_programs as test_programs
-import pmagpy_tests.test_demag_gui as test_demag_gui
-import pmagpy_tests.test_magic_gui2 as test_magic_gui2
-import pmagpy_tests.test_contribution_builder as test_contribution_builder
-import pmagpy_tests.test_find_pmag_dir as test_find_pmag_dir
-import pmagpy_tests.test_map_magic as test_map_magic
+from pmagpy_tests import test_ipmag
+from pmagpy_tests import test_magic_gui
+from pmagpy_tests import test_pmag
+from pmagpy_tests import test_pmag_gui
+from pmagpy_tests import test_thellier_gui
+from pmagpy_tests import test_validations
+from pmagpy_tests import test_programs
+from pmagpy_tests import test_demag_gui
+from pmagpy_tests import test_magic_gui2
+from pmagpy_tests import test_contribution_builder
+from pmagpy_tests import test_find_pmag_dir
+from pmagpy_tests import test_map_magic
 
 __all__ = [test_builder, test_er_magic_dialogs, test_ipmag,
            test_magic_gui, test_pmag, test_pmag_gui, test_thellier_gui,
