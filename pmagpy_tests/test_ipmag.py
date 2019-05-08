a6945537af5d96609dfd8e954ddce3069d66d9a9
Lori J <mintblue87@gmail.com>
2019-05-08 11:13:22 -0700

    move dmag_magic function into ipmag, #572

--- pmagpy_tests/test_ipmag.py
+++ pmagpy_tests/test_ipmag.py
@@ -1220,6 +1220,50 @@ class TestDayplotMagic(unittest.TestCase):
         self.assertTrue(glob.glob("*.png"))
 
 
+class TestDmagMagic(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        glob_strings = ["*.png", "*.svg", "data_files/3_0/McMurdo/*.png"]
+        for string in glob_strings:
+            for fname in glob.glob(string):
+                os.remove(fname)
+
+    def test_success(self):
+        res, outfiles = ipmag.dmag_magic(dir_path=".", input_dir_path="data_files/3_0/McMurdo", LT="T", plot_by='sit')
+        self.assertTrue(res)
+        for f in outfiles:
+            self.assertTrue(os.path.exists(f))
+        images = glob.glob("*.svg")
+        self.assertEqual(len(images), 126)
+
+    def test_alt_success(self):
+        res, outfiles = ipmag.dmag_magic(dir_path=".", input_dir_path="data_files/3_0/McMurdo", plot_by='spc', fmt="png")
+        self.assertTrue(res)
+        for f in outfiles:
+            self.assertTrue(os.path.exists(f))
+        images = glob.glob("*.png")
+        self.assertEqual(len(images), 530)
+
+    def test_with_output_dir(self):
+        res, outfiles = ipmag.dmag_magic(dir_path="data_files/3_0/McMurdo", plot_by='loc', fmt="png")
+        self.assertTrue(res)
+        for f in outfiles:
+            self.assertTrue(os.path.exists(f))
+        images = glob.glob("data_files/3_0/McMurdo/*.png")
+        self.assertEqual(len(images), 1)
+
+
+    def test_failure(self):
+        res, outfiles = ipmag.dmag_magic(dir_path=".", input_dir_path="data_files/3_0/McMurdo", plot_by='spc',
+                                        fmt="png", LT="FAKE")
+        self.assertFalse(res)
+
+    def test_alt_failure(self):
+        res, outfiles = ipmag.dmag_magic(dir_path=".", input_dir_path="data_files/3_0/", plot_by='spc')
+        self.assertFalse(res)
 
 
 if __name__ == '__main__':
