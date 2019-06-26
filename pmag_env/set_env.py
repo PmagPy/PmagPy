b158ff1707c8e9d1aa7a90cf39d8a3b52608d545
Lori J <mintblue87@gmail.com>
2019-06-26 13:05:29 -0700

    add IS_LINUX constant to set_env

--- pmag_env/set_env.py
+++ pmag_env/set_env.py
@@ -4,6 +4,7 @@ import os
 isServer = False
 verbose = True
 IS_WIN = True if sys.platform in ['win32', 'win64'] else False
+IS_LINUX = True if 'linux' in sys.platform else False
 IS_FROZEN = getattr(sys, 'frozen', False)
 IS_NOTEBOOK = True if 'JPY_PARENT_PID' in os.environ else False
 OFFLINE = False
