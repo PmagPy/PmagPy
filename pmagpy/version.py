0616fe857e25f31a4c0ecd5bc680fd7c5644767c
Lori J <mintblue87@gmail.com>
2019-06-27 12:56:06 -0700

    update all notebooks and version number

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -4,5 +4,5 @@ Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
 
-"pmagpy-4.2.26"
-version = 'pmagpy-4.2.26'
+"pmagpy-4.2.27"
+version = 'pmagpy-4.2.27'
