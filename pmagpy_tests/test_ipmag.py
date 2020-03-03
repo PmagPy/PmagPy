fe16b513ad0b67855a4f8ff36a62f14a521aeedd
Lori J <mintblue87@gmail.com>
2020-03-03 15:49:37 -0800

    fix up tests for downloading from MagIC, include wget module and remove unneeded test

--- pmagpy_tests/test_ipmag.py
+++ pmagpy_tests/test_ipmag.py
@@ -186,18 +186,6 @@ class TestDownloadMagic(unittest.TestCase):
                 self.assertIn(f, output_files)
             shutil.rmtree('temp')
 
-    def test_download_from_magic(self):
-        tables = ['measurements.txt', 'specimens.txt', 'samples.txt',
-                  'locations.txt', 'ages.txt',
-                  'contribution.txt']
-        res = ipmag.download_from_magic(12366)
-        self.assertTrue(res)
-        for fname in tables:
-            self.assertTrue(os.path.exists(fname))
-        pmag.remove_files(tables, ".")
-        res = ipmag.download_from_magic(123)
-        self.assertFalse(res)
-
 
     def test_all_files_are_created(self):
         files = ['locations.txt', 'sites.txt', 'samples.txt', 'specimens.txt',
