03fcf652a2dd698e352ce1e464a7c6170c9d3b9a
Lisa Tauxe <ltauxe@ucsd.edu>
2020-03-04 13:55:55 -0800

    modified:   pmagpy/version.py

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -4,5 +4,5 @@ Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
 
-"pmagpy-4.2.37"
-version = 'pmagpy-4.2.37'
+"pmagpy-4.2.38"
+version = 'pmagpy-4.2.38'
