ae6ac95c13fa0d80b251a5794904489e91d16227
Nick Jarboe <njarboe@gmail.com>
2020-07-30 14:05:12 -0700

    added to comments

--- programs/conversion_scripts/run_multi_samples.py
+++ programs/conversion_scripts/run_multi_samples.py
@@ -8,11 +8,30 @@ def main():
         run_multi_samples.py
     
     DESCRIPTION
-        For SQUID microscope files that have multiple samples, this program runs the 
+        This program is for SQUID microscope files that have multiple samples. The program runs the 
         squidm_magic.py program using the command found in files named "commandSample_Name"
         placed in the top file directory. Then it combines the MagIC files into one MagIC 
         text file for uploading. One should run the program in the directory that has the 
-        command files and the sample directories.
+        command files and the sample directories. This program may be for other file formats in the future.
+
+        The top directory should have the sample directories and the command files. For example:
+
+        BorlinaEtAl2020 --> commandCong14c
+                            commandD175C
+                            commandD175H
+                            commandD175L
+                            Cong14c (sample name)
+                            D175C
+                            D175H
+                            D175L --> 150107_h18 --> data --> F1_115C_Z_150107_h18_map1.bz   (step raw data)
+                                     (specimen name)          F1_115C_Z_150107_h18_map1.inf  (step info file)
+                                                              F1_115C_Z_150107_h18_map1.fits (step model fit info)
+                                                     demag --> 150107_h18     (CIT data file) 
+                                                               150107_h18.sam (CIT sample list)
+                                                     images --> F1_115C_Z_150107_h18_map1.pdf (human readable image)
+                           
+        Run the program in the directory with the commands and sample directories (in the BorlinaEtAl2020 in this example).
+
 
     SYNTAX
         run_multi_samples.py
@@ -60,8 +79,6 @@ def main():
         samp_list=samp_list+sample+"/samples.txt "
         speci_list=speci_list+sample+"/specimens.txt "
         meas_list=meas_list+sample+"/measurements.txt "
-    print("loc_list=",loc_list)
-    print("site_list=",site_list)
     os.system("combine_magic.py -F locations.txt -f " + loc_list) 
     os.system("combine_magic.py -F sites.txt -f " + site_list) 
     os.system("combine_magic.py -F samples.txt -f " + samp_list) 
