ba0c4dbdf0c3a88207e43982610032a45da45f4b
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2020-11-06 08:08:41 -0800

    add option to change land_edge_color in ipmag.make_orthographic_map()

--- pmagpy/ipmag.py
+++ pmagpy/ipmag.py
@@ -1666,7 +1666,8 @@ def plot_di_mean_ellipse(dictionary, fignum=1, color='k', marker='o', markersize
 
 
 def make_orthographic_map(central_longitude=0, central_latitude=0, figsize=(8, 8),
-                          add_land=True, land_color='tan', add_ocean=False, ocean_color='lightblue', grid_lines=True,
+                          add_land=True, land_color='tan', land_edge_color='black',
+                          add_ocean=False, ocean_color='lightblue', grid_lines=True,
                           lat_grid=[-80.,  -60.,  -30.,
                                     0.,   30.,   60., 80.],
                           lon_grid=[-180., -150., -120.,  -90.,  -60.,  -30.,    0.,   30.,   60., 90.,  120.,  150.,  180.]):
@@ -1708,7 +1709,7 @@ def make_orthographic_map(central_longitude=0, central_latitude=0, figsize=(8, 8
         ax.add_feature(cartopy.feature.OCEAN, zorder=0, facecolor=ocean_color)
     if add_land == True:
         ax.add_feature(cartopy.feature.LAND, zorder=0,
-                       facecolor=land_color, edgecolor='black')
+                       facecolor=land_color, edgecolor=land_edge_color)
     if grid_lines == True:
         ax.gridlines(xlocs=lon_grid, ylocs=lat_grid, linewidth=1,
                      color='black', linestyle='dotted')
