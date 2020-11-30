858fd530b47ddd1b5b40c0e4c20c355984b508a1
Lisa Tauxe <ltauxe@ucsd.edu>
2020-11-30 13:22:33 -0800

    modified:   PmagPy_online.ipynb 	deleted:    PmagPy_online/PmagPy_for_Geomag/BigMovie.gif 	deleted:    PmagPy_online/PmagPy_for_Geomag/Bmovie.gif 	modified:   pmagpy/version.py

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.2.54"
-version = 'pmagpy-4.2.54'
+"pmagpy-4.2.55"
+version = 'pmagpy-4.2.55'
