4b6b2ec0545fb9d5db8c0f2b516ab5b92022f918
Lisa Tauxe <ltauxe@ucsd.edu>
2020-12-19 15:39:46 -0800

    modified:   pmagpy/convert_2_magic.py 	modified:   pmagpy/version.py

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.2.57"
-version = 'pmagpy-4.2.57'
+"pmagpy-4.2.58"
+version = 'pmagpy-4.2.58'
