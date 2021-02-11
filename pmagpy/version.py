ca71cd3d539fd5d1326ee26bb74478cf6613531c
Lisa Tauxe <ltauxe@ucsd.edu>
2021-02-11 15:43:03 -0800

    modified:   ipmag.py # fixed ipmag.atrm_magic 	modified:   version.py

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.2.68"
-version = 'pmagpy-4.2.68'
+"pmagpy-4.2.69"
+version = 'pmagpy-4.2.69'
