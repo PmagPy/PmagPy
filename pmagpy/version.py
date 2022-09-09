0181105d8d70920c813f72617e2df349e49b2bdb
Lisa Tauxe <ltauxe@ucsd.edu>
2022-09-09 16:09:25 -0700

    modified:   pmagpy/version.py

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.2.106"
-version = 'pmagpy-4.2.106'
+"pmagpy-4.2.107"
+version = 'pmagpy-4.2.107'
