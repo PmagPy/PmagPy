de6cc67d5c1da67f039a5fa319d0e2ff7034d56d
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2022-10-17 08:33:07 -0700

    iterate version number for pypi release

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.2.107"
-version = 'pmagpy-4.2.107'
+"pmagpy-4.2.108"
+version = 'pmagpy-4.2.108'
