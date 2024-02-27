4c72b05c99817f18d87c82d41af664538375c711
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2024-02-26 16:15:54 -0800

    bump up version number before pip release

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.2.115"
-version = 'pmagpy-4.2.115'
+"pmagpy-4.2.116"
+version = 'pmagpy-4.2.116'
