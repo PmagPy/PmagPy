b75fda7511534a88df1a1415adc1ceca1b3c5c4e
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2024-05-29 08:56:24 -0700

    increment version to enable pip release with fixed ipmag.download_magic_from_id

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.2.120"
-version = 'pmagpy-4.2.120'
+"pmagpy-4.2.121"
+version = 'pmagpy-4.2.121'
