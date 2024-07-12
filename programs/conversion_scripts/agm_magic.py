6541bd44b40326fc9431ce3dcbf8c25d44ff07ee
Nick Jarboe <njarboe@gmail.com>
2024-07-12 14:50:17 -0600

    added naming convention help text

--- programs/conversion_scripts/agm_magic.py
+++ programs/conversion_scripts/agm_magic.py
@@ -26,6 +26,20 @@ OPTIONS
     -Flo LOCFILE, specify locations.txt file; default is SPEC_locations.txt
     -old : infile is in old format
 
+    NOTES:
+     Sample naming convention:
+    [1] XXXXY: where XXXX is an arbitrary length site designation and Y
+        is the single character sample designation.  e.g., TG001a is the
+        first sample from site TG001.    [default]
+    [2] XXXX-YY: YY sample from site XXXX (XXX, YY of arbitary length)
+    [3] XXXX.YY: YY sample from site XXXX (XXX, YY of arbitary length)
+    [4-Z] XXXX[YYY]:  YYY is sample designation with Z characters from site XXX
+    [5] site name = sample name
+    [6] site name entered in sitename column in the orient.txt format input file -- NOT CURRENTLY SUPPORTED
+    [7-Z] [XXX]YYY:  XXX is site designation with Z characters from samples  XXXYYY
+    NB: all others you will have to either customize your
+        self or e-mail ltauxe@ucsd.edu for help.
+
 OUTPUT
     MagIC format files: measurements, specimens, sample, site
 """
