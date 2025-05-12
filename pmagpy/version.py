6c0df46fda45f0a5120b9439a6d54478eaa4241d
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2025-05-12 12:49:32 -0500

    step up version to 4.3.0 for new pip release with rockmagpy

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.2.125"
-version = 'pmagpy-4.2.125'
+"pmagpy-4.3.0"
+version = 'pmagpy-4.3.0'
