73056044d5f6ea363e2f202e98da9fa0807c91fd
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2025-06-09 13:59:09 -0500

    step up version for another rockmagpy JupyterHub test session

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.3.3"
-version = 'pmagpy-4.3.3'
+"pmagpy-4.3.4"
+version = 'pmagpy-4.3.4'
