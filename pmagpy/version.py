558c7319803547a017e423460e510c4c98db0cd7
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2026-02-04 16:23:54 -0600

    step-up version for new hyst processing functionality

--- pmagpy/version.py
+++ pmagpy/version.py
@@ -3,5 +3,5 @@ Module contains current pmagpy version number.
 Version number is displayed by GUIs
 and used by setuptools to assign number to pmagpy/pmagpy-cli.
 """
-"pmagpy-4.3.10"
-version = 'pmagpy-4.3.10'
+"pmagpy-4.3.11"
+version = 'pmagpy-4.3.11'
